d511499cb72f36c356714eb6e0ef952f8b79e0e3
Shu Uesugi <shu@chibicode.com>
2019-02-01 12:54:48 -0800

    :pencil:

--- src/components/from-book.js
+++ src/components/from-book.js
@@ -4,7 +4,7 @@ import Tooltip from '@material-ui/core/Tooltip'
 import colors from '../lib/colors'
 
 const FromBook = props => (
-  <Tooltip title="本の脚注にもあります" enterDelay={600}>
+  <Tooltip title="本の脚注からの引用" enterDelay={600}>
     <span
       css={css`
         border-bottom: 1px dashed ${colors.green400};
