e89f8c4552dd1933adbec9b0efdfa0d15ddfd4c5
Shu Uesugi <shu@chibicode.com>
2019-02-01 12:01:33 -0800

    Update amazon-link.js

--- src/components/amazon-link.js
+++ src/components/amazon-link.js
@@ -3,7 +3,7 @@ import React from 'react'
 // Not using an affiliate link
 const AmazonLink = props => (
   <a
-    href="¨https://www.amazon.co.jp/dp/B07LG7TG5N/ref=cm_sw_r_tw_dp_U_x_kZQuCbNDHEY8V"
+    href="https://www.amazon.co.jp/dp/B07LG7TG5N/ref=cm_sw_r_tw_dp_U_x_kZQuCbNDHEY8V"
     target="_blank"
     rel="noopener noreferrer"
     {...props}
