f84348e10ae5e37f41264e7ee46300b3ea466fa6
Shu Uesugi <shu@chibicode.com>
2019-02-02 10:25:34 -0800

    Add pagenumber

--- src/components/from-book.js
+++ src/components/from-book.js
@@ -3,8 +3,12 @@ import {jsx, css} from '@emotion/core'
 import Tooltip from '@material-ui/core/Tooltip'
 import colors from '../lib/colors'
 
-const FromBook = props => (
-  <Tooltip title="本の脚注からの引用" enterDelay={600}>
+const FromBook = ({pageNumber, ...props}) => (
+  <Tooltip
+    title={`本の脚注${pageNumber && `(${pageNumber}ページ)`}からの引用`}
+    enterDelay={600}
+    placement="top"
+  >
     <span
       css={css`
         border-bottom: 1px dashed ${colors.green400};
