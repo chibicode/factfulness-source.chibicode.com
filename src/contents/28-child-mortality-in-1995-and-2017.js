d26b535bc35e44b6f052a64e2126113cb8ed5220
Shu Uesugi <shu@chibicode.com>
2019-02-07 09:30:36 -0800

    Update 28-child-mortality-in-1995-and-2017.js

--- src/contents/28-child-mortality-in-1995-and-2017.js
+++ src/contents/28-child-mortality-in-1995-and-2017.js
@@ -4,14 +4,15 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      Child mortality data used in the 1995 lecture came from UNICEF[1]. In this
-      book we have updated the examples, using 2017 estimations from
-      <a href="http://www.childmortality.org/">UN-IGME</a>---a data
-      collaboration between UNICEF, WHO, UN Population Division and the World
-      Bank. The numbers used in the evening lecture back in 1995 were only
-      slightly different: Saudi Arabia in 1960 was 292 and in 1993, 38; Malaysia
-      in 1960 was 105 and in 1993, 17; Brazil in 1960 was 181 and in 1993, 63;
-      Tanzania in 1960 was 249 and in 1993, 16.
+      <H.FB pageNumber={370}>
+        1995年の講義で使った乳幼児死亡率はユニセフによるもの(UNICEF[1])。本書では当初よりも新しい2017年のデータを使っている(
+        <a href="http://www.childmortality.org/">UN-IGME</a>)
+      </H.FB>
+      ---a data collaboration between UNICEF, WHO, UN Population Division and
+      the World Bank. The numbers used in the evening lecture back in 1995 were
+      only slightly different: Saudi Arabia in 1960 was 292 and in 1993, 38;
+      Malaysia in 1960 was 105 and in 1993, 17; Brazil in 1960 was 181 and in
+      1993, 63; Tanzania in 1960 was 249 and in 1993, 16.
     </p>
     <p>
       The data from UNICEF[1] is based on The State of the World's Children
@@ -19,8 +20,8 @@ export default () => (
       <a href="https://www.unicef.org/about/history/files/sowc_1995.pdf">
         PDF here
       </a>
-      . Data for 2017 is derived from &quot;Child Mortality Estimates&quot;
-      from <a href="http://www.childmortality.org/">www.childmortality.org</a>.
+      . Data for 2017 is derived from &quot;Child Mortality Estimates&quot; from{' '}
+      <a href="http://www.childmortality.org/">www.childmortality.org</a>.
       UN-IGME---United Nations Inter-agency Group for Child Mortality
       Estimation---is a data collaboration between UNICEF, WHO, UN Population
       Division and the World Bank.
