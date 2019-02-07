69ffd0874e454353fde9f7094b477cd9f81908b5
Shu Uesugi <shu@chibicode.com>
2019-02-07 10:50:25 -0800

    Completion rate

--- src/contents/40-primary-school-completion-rate.js
+++ src/contents/40-primary-school-completion-rate.js
@@ -3,17 +3,22 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The primary school completion rate for girls is below 35 percent in just
-    three countries. But for all three, the uncertainty is high and the numbers
-    are outdated: Afghanistan (1993), 15 percent; South Sudan (2011), 18
-    percent; Chad (2011), 30 percent. Three other countries---Somalia, Syria,
-    and Libya---have no official number. The girls in these six countries suffer
-    under severe gender inequality, but in total they make up only 2 percent of
-    all girls of primary school age in the world. The data is based on
-    UN-Pop[4], &quot;Annual population by age---Female, medium fertility
-    variant,&quot; available at
-    <a href="http://www.gapm.io/xpopage">www.gapm.io/xpopage</a>. Note that in
-    these countries, many boys are also missing school. See
+    <H.FB>
+      The primary school completion rate for girls is below 35 percent in just
+      three countries. But for all three, the uncertainty is high and the
+      numbers are outdated: Afghanistan (1993), 15 percent; South Sudan (2011),
+      18 percent; Chad (2011), 30 percent. Three other countries---Somalia,
+      Syria, and Libya---have no official number. The girls in these six
+      countries suffer under severe gender inequality, but in total they make up
+      only 2 percent of all girls of primary school age in the world.
+    </H.FB>{' '}
+    The data is based on UN-Pop[4], &quot;Annual population by age---Female,
+    medium fertility variant,&quot; available at
+    <a href="http://www.gapm.io/xpopage">www.gapm.io/xpopage</a>.{' '}
+    <H.FB>
+      Note that in these countries, many boys are also missing school.
+    </H.FB>{' '}
+    See
     <a href="http://gapm.io/twmedu">gapm.io/twmedu</a>.
   </p>
 )
