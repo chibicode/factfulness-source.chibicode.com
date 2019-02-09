2e275218cc33e4b7f172b0200af2b138d23a1aff
Shu Uesugi <shu@chibicode.com>
2019-02-08 18:06:14 -0800

    :pencil:

--- src/contents/67-dip-in-extreme-poverty-china-india-and-latin-america.js
+++ src/contents/67-dip-in-extreme-poverty-china-india-and-latin-america.js
@@ -3,9 +3,11 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The numbers in the text on the reductions in extreme poverty in China,
-    India, Latin America and elsewhere come from World Bank[5] extended to 2017
-    by <a href="http://gapm.io/depov">Gapminder[9]</a>, assuming IMF forecasts
-    are accurate.
+    <H.FB pageNumber={368}>
+      中国、インド、中南米、そしてその他の国で極度の貧困が減ったというデータは、World
+      Bank[5]によるもの。
+    </H.FB>
+    IMFの予測が正しいという前提のもと、わたしたちは2017年までのデータを算出した(
+    <H.A href="http://gapm.io/depov">Gapminder[9]</H.A>)。
   </p>
 )
--- src/contents/69-life-expectancy-and-data-doubt.js
+++ src/contents/69-life-expectancy-and-data-doubt.js
@@ -3,14 +3,14 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Life expectancy data is from <a href="http://gapm.io/xihlex">IHME[1]</a>
-    (Institute for Health Metrics and Evaluation). In 2016, only the Central
-    African Republic and Lesotho had a life expectancy as low as 50 years. But
-    uncertainties are huge, especially on Level 1 and 2. Learn how much data
-    doubt you should have at
-    <a href="https://www.gapminder.org/data/documentation/gd004/">
-      https://www.gapminder.org/data/documentation/gd004/
-    </a>
-    .
+    <H.FB pageNumber={368}>
+      平均寿命のデータは保健指標評価研究所によるもの(
+      <H.A href="http://gapm.io/xihlex">IHME[1]</H.A>
+      )。2016年に平均寿命が50歳近くだったのはレソトと中央アフリカ共和国のみ。しかし、特にレベル1と2の国においては、データはとても不確かだ。データをどこまで疑うべきかは、
+      <H.A href="https://www.gapminder.org/data/documentation/gd004/">
+        こちらを参考にしてほしい
+      </H.A>
+      。
+    </H.FB>
   </p>
 )
