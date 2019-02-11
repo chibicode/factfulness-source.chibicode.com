efd10fb326ab63a306c1b7273b3771545dbc214b
Shu Uesugi <shu@chibicode.com>
2019-02-11 12:23:36 -0800

    Update 138-disaster-data.js

--- src/contents/138-disaster-data.js
+++ src/contents/138-disaster-data.js
@@ -7,10 +7,15 @@ export default () => (
     earthquake in Nepal 2015 killed 9,034, injured 200,000 and affected 5.6
     million people. The government of Nepal estimates a slightly higher death
     toll of 10,000 deaths; see PDNA. To avoid underestimating the suffering, we
-    have used the higher number from PDNA. Numbers for the 2003 heat wave in
-    Europe are from UNISDR, estimating the total death toll for Western Europe
-    to be 46,730 fatalities. All other disaster data used here comes from
-    EM-DAT. Nowadays, Bangladesh has a very advanced flood-monitoring website;
-    see http://www.ffwc.gov.bd.
+    have used the higher number from PDNA.{' '}
+    <H.FB pageNumber={365}>
+      2003年のヨーロッパの熱波についてのデータはUNISDRによるもの。
+    </H.FB>
+    estimating the total death toll for Western Europe to be 46,730 fatalities.
+    その他の災害のデータはEM-DATによるもの。
+    <H.FB pageNumber={365}>
+      現在バングラデシュで洪水対策に使われている、すばらしいデジタル監視システムは
+      <H.A href="http://www.ffwc.gov.bd">こちら</H.A>。
+    </H.FB>
   </p>
 )
