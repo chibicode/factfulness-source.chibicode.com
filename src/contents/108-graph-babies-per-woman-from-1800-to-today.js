42ef65fa0114fa1cfd7f510e9249580bda399439
Shu Uesugi <shu@chibicode.com>
2019-02-11 11:26:56 -0800

    Update 108-graph-babies-per-woman-from-1800-to-today.js

--- src/contents/108-graph-babies-per-woman-from-1800-to-today.js
+++ src/contents/108-graph-babies-per-woman-from-1800-to-today.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import * as H from '../components/h'
 
+// マティアス・リンドグレーン
 export default () => (
   <>
     <p>
-      We use the term &quot;babies per woman&quot; for the statistical indicator
-      &quot;total fertility rate&quot; (TFR). Fertility rates decline during
-      times of hardship. The dips and humps before 1965 shown on the graph were
-      caused by the Second World War and famines. Fertility climbs to rates
-      higher than usual when the crisis is over. Globally, the average fertility
-      rate was at 5.05 children in 1950, according to UN-Pop[3]. As of 2017, the
-      estimated fertility rate was 2.48 children, according to UN-Pop.
-      Replacement rate with the current mortality worldwide is 2.3.
+      <H.FB pageNumber={366}>
+        本書では「合計特殊出生率」という専門用語を「女性ひ
+        とりあたりの子供の数」と表記している
+      </H.FB>
+      Fertility rates decline during times of hardship. The dips and humps
+      before 1965 shown on the graph were caused by the Second World War and
+      famines. Fertility climbs to rates higher than usual when the crisis is
+      over. Globally, the average fertility rate was at 5.05 children in 1950,
+      according to UN-Pop[3]. As of 2017, the estimated fertility rate was 2.48
+      children, according to UN-Pop. Replacement rate with the current mortality
+      worldwide is 2.3.
     </p>
     <p>
       The long trend displayed on the graph shows Mattias Lindgren's work that
       compiled multiple historical sources for the years before 1950. It is
       aligned with UN-Pop[3] estimates for post-1950; see
-      <a href="http://gapm.io/dtfr">Gapminder[7]</a>. The dashed line after 2017
-      shows the UN-Pop[1] medium fertility projection, which is expected to
-      reach 1.96 in 2099.
+      <a href="http://gapm.io/dtfr">Gapminder[7]</a>.{' '}
+      <H.FB pageNumber={366}>
+        2017 年から先の点線は国連 の出生率中位予測。2099
+        年には女性ひとりあたりの子供の数は 1. 96 になると予測されて いる
+      </H.FB>
     </p>
     <p>
       For more about famines before 1965, see, for example, the
