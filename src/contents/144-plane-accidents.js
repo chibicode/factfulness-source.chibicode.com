e987e025ae4c2107f1cb6f864e6594bad6336f32
Shu Uesugi <shu@chibicode.com>
2019-02-11 12:58:44 -0800

    Update 144-plane-accidents.js

--- src/contents/144-plane-accidents.js
+++ src/contents/144-plane-accidents.js
@@ -4,16 +4,23 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      The data on fatalities in recent years is from IATA. The data on passenger
-      miles is from the UN agency that managed to reduce the number of
-      accidents, based on ICAO[1,2,3]. Gapminder[16] on airplane fatalities---v1
-      is based on<a href="http://gapm.io/xiatas"> IATA</a>,
+      <H.FB pageNumber={365}>
+        最近の飛行機事故の死亡者数は国際航空運送協会のデータを利用した(
+        <H.A href="http://gapm.io/xiatas">IATA</H.A>
+        )。旅客機の飛行距離のデータは、飛行機事故を減らす目的でつくられた国連の
+        専門機関である、国際民間航空機関のデータを利用した(
+        <H.A href="https://www.icao.int/sustainability/Pages/Air-Traffic-Monitor.aspx">
+          ICAO[1,2,3]
+        </H.A>
+        )
+      </H.FB>{' '}
+      Gapminder[16] on airplane fatalities---v1 is based on
       <a href="https://www.icao.int/sustainability/Pages/Air-Traffic-Monitor.aspx">
         ICAO[3]
       </a>
       , <a href="http://www.gapm.io/xbtsafat">US Air Carrier Safety Data</a>
-       and <a href="http://www.gapm.io/xbtspass">Revenue Passenger-Miles</a>
-       from BTS[1,2] and The Annual Reports of the U.S. Scheduled Airline
+      and <a href="http://www.gapm.io/xbtspass">Revenue Passenger-Miles</a>
+      from BTS[1,2] and The Annual Reports of the U.S. Scheduled Airline
       Industryfrom <a href="http://airlines.org/">ATAA</a>. See
       <a href="http://www.gapm.io/ttranspa">www.gapm.io/ttranspa</a> and
       <a href="http://www.gapm.io/dpland">www.gapm.io/dpland</a>.
