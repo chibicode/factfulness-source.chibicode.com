1e4bbdea39ae1acd288ec569d64f4715dd52f79a
Shu Uesugi <shu@chibicode.com>
2019-02-11 14:19:20 -0800

    Update 150-refusing-vaccination.js

--- src/contents/150-refusing-vaccination.js
+++ src/contents/150-refusing-vaccination.js
@@ -3,16 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    In the US, 4 percent of parents think that vaccines are not important,
-    according to <a href="http://gapm.io/xgalvac17.">Gallup[3]</a>. In 2016,
-    Larson et al. found that, across 67 countries, an average of 13 percent of
-    people were skeptical about vaccination in general. There were huge
-    variations between countries: from more than 35 percent in France and Bosnia
-    and Herzegovina to 0 percent in Saudi Arabia and Bangladesh. In 1990,
-    measles was the cause of 7 percent of all child deaths. Today, thanks to
-    vaccination, it is only 1 percent. Deaths from measles mainly happen on
-    Level 1 and Level 2, where children only recently started to get vaccinated;
-    see <a href="http://gapm.io/xihels">IHME[7]</a> and
-    <a href="http://gapm.io/xwhoim">WHO[1]</a>.
+    アメリカでは、子供がいる親の4%が、予防接種は大事ではないと考えている(
+    <H.A href="http://gapm.io/xgalvac17">Gallup[3]</H.A>)。またLarson et
+    alが2016年に67カ国を対象に行った調査によると、67カ国に暮らす人のうち13%が、予防接種に懐疑的だという。国ごとのばらつきは大きかった。フランスやボスニア・ヘルツェゴビナでは35%以上が懐疑的だったが、サウジアラビアやバングラデシュでは0%だった。1990年には、はしかが子供が命を落とす原因の7%だった。しかしワクチンのおかげで、現在は1%になった。また現在、はしかで亡くなる子供の多くはレベル1や2の国の子供だ。予防接種が最近になるまで普及しなかったからだ(
+    <H.A href="http://gapm.io/xihels">IHME[7]</H.A>、
+    <H.A href="http://gapm.io/xwhoim">WHO[1]</H.A>)。
   </p>
 )
