d6236c91d0d6e68a07feedec94d4b8768aaffa0b
Shu Uesugi <shu@chibicode.com>
2019-02-11 12:08:42 -0800

    Update 119-graphs-14-straight-lines-s-bends-slides-and-humps.js

--- src/contents/119-graphs-14-straight-lines-s-bends-slides-and-humps.js
+++ src/contents/119-graphs-14-straight-lines-s-bends-slides-and-humps.js
@@ -7,15 +7,22 @@ export default () => (
     two indicators against each other and then drawing a line in the
     middle---just like the World Health Chart you see in the beginning of the
     book. We removed the background bubbles in the small images, because it got
-    too cluttered. Most of these charts use national averages, aggregated by the
-    national income level; see <a href="http://gapm.io/elev">Gapminder[3]</a>. A
-    few (the straight line on recreational spending, the S-bend on vaccinations
-    and fridges, and the slide on fertility) use household data. Our estimates
-    of typical families on different income levels are not based on country
-    averages, because that would severely underestimate the number of the
-    poorest and mask the wide range of differences for countries on levels 1--3.
-    Very few countries follow these lines exactly, but the lines show the
-    general pattern of all countries over several decades. In each example,
-    there are huge differences between countries on every level.
+    too cluttered.
+    <H.FB pageNumber={365}>
+      これらのグラフの多くは国ごとの所得データを基に作成している(
+      <H.A href="http://gapm.io/elev">Gapminder[3]</H.A>
+      )。趣味への支出を表した直線のグラフ、
+      予防接種や冷蔵庫を表したS字カーブのグラフ、女性ひとりあたりの子供の数を表したすべり台のグラフは家庭ごとのデータを基に作成している。
+    </H.FB>
+    Our estimates of typical families on different income levels are not based
+    on country averages, because that would severely underestimate the number of
+    the poorest and mask the wide range of differences for countries on levels
+    1--3.
+    <H.FB pageNumber={365}>
+      また、一つひとつ国のレベル
+      が上がるにつれて、グラフとまったく同じ方向に数字が変化することはほとんどない。
+      しかし、すべての国の数十年間の変化を平均すると、グラフの形はだいたい当てはまる。どのグラフでも、同じ所
+      得レベルに属する国のあいだにはとても大きな差がある。
+    </H.FB>
   </p>
 )
