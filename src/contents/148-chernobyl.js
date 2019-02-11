79ee75c3c9319a4fdf09b4827b92f17a24cda15d
Shu Uesugi <shu@chibicode.com>
2019-02-11 13:44:51 -0800

    Update 148-chernobyl.js

--- src/contents/148-chernobyl.js
+++ src/contents/148-chernobyl.js
@@ -4,25 +4,30 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      The discussion of health investigations after Chernobyl is based on
-      <a href="http://gapm.io/xwhoc30">WHO[5]</a>. In 2016, the World Health
-      Organization published &quot;1986-2016: Chernobyl at 30 -- an update&quot;
-      and they report that they couldn't confirm any large increase in cancers,
-      like thyroid cancer, that many had expected in the population in general.
-      Instead, they conclude: &quot;The psycho-social impact of disasters and
-      emergencies has been well documented. It has been reported to be the
-      Chernobyl accident main public health impact that affected the largest
-      number of people. A similar effect is now reported in the aftermath of the
-      Fukushima disaster, &quot; reporting that &quot;anxiety levels that were
-      twice as high than non-exposed population, and were more likely to report
-      multiple unexplained physical symptoms and subjective poor health.&quot;
-      Read more about WHO's summarizing reports on the healths effects of the
-      Chernobyl accident, here:
+      <H.FB pageNumber={364}>
+        チェルノブイリの調査についてはWHO[5]を参照のこと
+      </H.FB>
+      In 2016, the World Health Organization published &quot;1986-2016:
+      Chernobyl at 30 -- an update&quot; and they report that they couldn't
+      confirm any large increase in cancers, like thyroid cancer, that many had
+      expected in the population in general. Instead, they conclude: &quot;The
+      psycho-social impact of disasters and emergencies has been well
+      documented. It has been reported to be the Chernobyl accident main public
+      health impact that affected the largest number of people. A similar effect
+      is now reported in the aftermath of the Fukushima disaster, &quot;
+      reporting that &quot;anxiety levels that were twice as high than
+      non-exposed population, and were more likely to report multiple
+      unexplained physical symptoms and subjective poor health.&quot; Read more
+      about WHO's summarizing reports on the healths effects of the Chernobyl
+      accident, here:
       <a href="http://gapm.io/xwhoc30">http://gapm.io/xwhoc30</a>.
     </p>
     <p>
-      Data about nuclear warheads are from the website
-      <a href="http://www.gapm.io/xnuno">Nuclear Notebook</a>.
+      <H.FB pageNumber={364}>
+        核弾頭の数については
+        <H.A href="http://www.gapm.io/xnuno">Nuclear Notebook</H.A>
+        を参考にした。
+      </H.FB>
     </p>
   </>
 )
