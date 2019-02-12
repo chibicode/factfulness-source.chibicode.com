b992e72e1cee93275d8b604f9ef4df9241a0677f
Shu Uesugi <shu@chibicode.com>
2019-02-11 21:43:35 -0800

    Update 174-tuberculosis-tb-and-the-swine-flu.js

--- src/contents/174-tuberculosis-tb-and-the-swine-flu.js
+++ src/contents/174-tuberculosis-tb-and-the-swine-flu.js
@@ -3,13 +3,17 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The data on swine flu comes from
-    <a href="http://www.who.int/csr/disease/swineflu/updates/en/index.html">
-      WHO[17]
-    </a>
-    and the data on tuberculosis are based on
-    <a href="http://www.who.int/gho/tb/">Global Health Observatory data</a> and
-    <a href="http://www.gapm.io/xmdrtb">www.gapm.io/xmdrtb</a> from WHO[10,11].
+    <H.FB pageNumber={362}>
+      豚インフルエンザのデータは
+      <H.A href="http://www.who.int/csr/disease/swineflu/updates/en/index.html">
+        WHO[17]
+      </H.A>
+      、結核のデータはWHO[10](
+      <H.A href="http://www.who.int/gho/tb/">
+        Global Health Observatory data
+      </H.A>
+      )と<H.A href="http://www.gapm.io/xmdrtb">WHO[11]</H.A>によるもの。
+    </H.FB>
     For the risk for multidrug resistant TB strains, see WHO[17].
   </p>
 )
