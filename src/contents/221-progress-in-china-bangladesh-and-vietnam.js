e5a193dce75de49228ac75a2be9b13180337c363
Shu Uesugi <shu@chibicode.com>
2019-02-11 22:47:06 -0800

    Update 221-progress-in-china-bangladesh-and-vietnam.js

--- src/contents/221-progress-in-china-bangladesh-and-vietnam.js
+++ src/contents/221-progress-in-china-bangladesh-and-vietnam.js
@@ -3,14 +3,11 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The Population Bomb by Paul and Anne Ehrlich (1968) contributed to a
-    widespread idea that Asia and Africa would never be able to feed their
-    growing populations. A total of 3.5 million people died in the so-called
-    Bengal famine in 1942--1943: 2 million in Bangladesh and 1.5 million in
-    India, based on the territorial boundaries today. The data on deaths from
-    famines is from the International Disaster Database, EM-DAT. The Peace
-    Research Institute Oslo (PRIO) produces maps of conflicts and poverty:
-    <a href="http://www.gapm.io/mpoco">www.gapm.io/mpoco</a>. For global textile
-    production, see <a href="http://www.gapm.io/tmante">www.gapm.io/tmante</a>.
+    <H.FB pageNumber={361}>
+      「アジアとアフリカ諸国がますます増加する人口を養っていくことはできない」という考え方が広まったのは、ポール・エーリックとアン・エーリックの共著による『人口が爆発する!環境・資源・経済の観点から』(1994年、水谷美穂訳、新曜社)がきっかけだった。飢餓による死者数のデータは、世界的な災害データベースのEM-DATをもとに算出した。紛争と貧困の地図を作製しているのはオスロの国際平和研究所(PRIO)。詳しくは
+      <H.A href="https://www.gapm.io/mpoco">こちら</H.A>
+      。グローバルな繊維製品の生産については
+      <H.A href="https://gapm.io/tmante">こちら</H.A>。
+    </H.FB>
   </p>
 )
