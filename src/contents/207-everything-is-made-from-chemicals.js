f845b208a918c9848a55a3a181119c401df32c88
Shu Uesugi <shu@chibicode.com>
2019-02-11 22:07:31 -0800

    Update 207-everything-is-made-from-chemicals.js

--- src/contents/207-everything-is-made-from-chemicals.js
+++ src/contents/207-everything-is-made-from-chemicals.js
@@ -3,13 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    People with chemophobia divide the world into &quot;natural&quot; (safe) and
-    &quot;chemical&quot; (industrial and harmful). The world's largest database
-    of defined chemical compounds sees it differently. CAS contains 132 million
-    organic and synthetic chemicals and their properties. It shows that toxicity
-    is not related to who produces the compound. Cobratoxin (CAS registry number
-    12584-83-7), for example, which is produced by nature, paralyzes your
-    nervous system until you can't breathe; see
+    <H.FB pageNumber={362}>
+      化学製品恐怖症の人たちは、世界を「天然物質(安全)」と「化学物質(工業製品で有害)」の2つに分けて考える。化学物質についての世界最大のデータベース(CAS)を見る限り、世界はそのようには分かれていない。CASには1億3200万の有機物質と合成物質が登録され、それらの特性が記載されている。このデータベースによると、物質の製造者と有害性には関係がない。たとえば、コブラトキシン(CAS登録番号12584-83-7)は天然の毒で、神経システムを麻痺させて最期に呼吸を止めてしまう
+    </H.FB>
+    see
     <a href="https://en.wikipedia.org/wiki/Cobratoxin#cite_note-pmid21999367-8">
       Del Brutto (2012)
     </a>
