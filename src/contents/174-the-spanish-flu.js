a53859facbfb0554c22187ce4631d2b3b8f020b6
Shu Uesugi <shu@chibicode.com>
2019-02-11 21:39:58 -0800

    Update 174-the-spanish-flu.js

--- src/contents/174-the-spanish-flu.js
+++ src/contents/174-the-spanish-flu.js
@@ -3,10 +3,11 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    In his book America's Forgotten Pandemic, Crosby (1989) estimated that the
-    Spanish flu caused 50 million deaths. The number is confirmed by Johnson and
-    Mueller (2002) and CDC[1]. The world population in 1918 was 1.84 billion,
-    which means this pandemic wiped out 2.7 percent of the entire global
-    population.
+    <H.FB pageNumber={363}>
+      アルフレッド・W・クロスビー著『史上最悪のインフルエンザ』(2009
+      年、西村秀一訳、みすず書房)によると、スペインかぜで亡くなったのは約5000万人。この数字は後にJohnson
+      and
+      Mueller(2002)とCDC[1]によって立証された。1918年の世界の人口は18億4000万人だった。スペインかぜは人類全体の2.7%の命を奪ったということになる。
+    </H.FB>
   </p>
 )
