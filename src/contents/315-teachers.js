21ed1c831f2ca887d616b74a35a2ace86edc66e5
Shu Uesugi <shu@chibicode.com>
2019-02-12 13:51:46 -0800

    Update 315-teachers.js

--- src/contents/315-teachers.js
+++ src/contents/315-teachers.js
@@ -3,8 +3,9 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Visit <a href="https://www.gapminder.org/teach">www.gapminder.org/teach</a>
-     to find our free teaching materials and join the community of teachers
-    promoting a fact-based worldview in their classrooms.
+    <H.FB pageNumber={355}>
+      学校で使える無料教材を探したり、事実に基づく世界の見方を教えている教師のコミュニティに入るには、
+      <H.A href="https://www.gapminder.org/teach">こちらを訪れてほしい</H.A>。
+    </H.FB>
   </p>
 )
