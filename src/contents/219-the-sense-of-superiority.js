6705ab29d061b9ef5fab9c119546de5840a072ec
Shu Uesugi <shu@chibicode.com>
2019-02-11 22:33:39 -0800

    Update 219-the-sense-of-superiority.js

--- src/contents/219-the-sense-of-superiority.js
+++ src/contents/219-the-sense-of-superiority.js
@@ -3,7 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    For more on the sense of superiority over other groups, see Haidt, The
-    Righteous Mind: Why Good People Are Divided by Politics and Religion (2012).
+    <H.FB pageNumber={361}>
+      ほかの集団に対する優越感についてもっと知りたい方は、ジョナサン・ハイト著『社会はなぜ左と右に分かれるのか─対立を超えるための道徳心理学』(2014年、高橋洋訳、紀伊國屋書店)をお読みいただきたい。
+    </H.FB>
   </p>
 )
