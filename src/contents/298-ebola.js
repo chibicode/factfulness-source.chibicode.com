f3b02c6a7729232569623661ddcffb6a99e326ef
Shu Uesugi <shu@chibicode.com>
2019-02-12 13:18:12 -0800

    Update 298-ebola.js

--- src/contents/298-ebola.js
+++ src/contents/298-ebola.js
@@ -3,9 +3,11 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The WHO[13] <a href="http://gapm.io/xebolawho">Ebola situation reports</a>
-     lists all situation reports produced to track the Ebola pandemic since
-    2014. They still show suspect cases, and the CDC[3] continues to use the
-    high estimates, which include suspected and unconfirmed cases.
+    <H.FB pageNumber={356}>
+      世界保健機構は、2014
+      年以来のエボラ出血熱のすべての発生事例を記録し報告書を公開している(WHO[13]:{' '}
+      <H.A href="http://gapm.io/xebolawho">Ebola situation reports</H.A>
+      )。世界保健機構のデータには「感染の疑い」のある患者も含まれる。また、アメリカ疾病予防管理センター(CDC[3])は引き続き、「感染の疑い」のある患者と「感染が確認されていない」患者の両方を含んだ、実際より多めに見える推定患者数を公開している。
+    </H.FB>
   </p>
 )
