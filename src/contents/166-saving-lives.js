0db0ffd1837591eeb1462a499075a99ef3694a08
Shu Uesugi <shu@chibicode.com>
2019-02-11 21:35:55 -0800

    Update 166-saving-lives.js

--- src/contents/166-saving-lives.js
+++ src/contents/166-saving-lives.js
@@ -3,12 +3,12 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The list of the low-cost, high-impact interventions that save the most lives
-    comes from UNICEF[2], which also set out the essential basic health care to
-    which all citizens should have access before public health budgets start
-    being spent on more advanced care. The publication from UNICEF[2] is called
-    &quot;Narrowing the Gaps---The Power of Investing in the Poorest
-    Children&quot; (2017), and is available for PDF download here:
+    <H.FB pageNumber={363}>
+      コストをかけずに、最も多くの命を助けることができる施策については、ユニセフを参考にした(UNICEF[2])。またユニセフは、高度な医療施設におカネを使う前に、どのような基本的な医療の仕組みをまず整備すべきか解説している。
+    </H.FB>
+    The publication from UNICEF[2] is called &quot;Narrowing the Gaps---The
+    Power of Investing in the Poorest Children&quot; (2017), and is available
+    for PDF download here:
     <a href="http://www.gapm.io/xunicef2">www.gapm.io/xunicef2</a>.
   </p>
 )
