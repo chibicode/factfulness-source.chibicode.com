d59e383f4b146391d01e9f38e88c799ba283b7fa
Shu Uesugi <shu@chibicode.com>
2019-02-12 10:50:27 -0800

    Update 245-plundered-natural-resources.js

--- src/contents/245-plundered-natural-resources.js
+++ src/contents/245-plundered-natural-resources.js
@@ -3,10 +3,14 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    For discussions about the commons and how to avoid exploitation, see The
-    Plundered Planet: Why We Must---and How We Can---Manage Nature for Global
-    Prosperity, by Paul Collier (2010). The numbers of threatened species
-    1996--2017 come from IUCN Red List[4] and can be downloaded as
-    <a href="http://gapm.io/xiucnr4">PDF</a>.
+    <H.FB pageNumber={359}>
+      共有資源とその濫用防止に関する議論は、ポール・コリアー著『収奪の星─天然資源と貧困削減の経済学』(2012年、村井章子訳、みすず書房)に詳しい。
+    </H.FB>
+    1996年から2017年の絶滅危惧種については
+    <H.FB pageNumber={359}>
+      国際自然保護連合のレッドリスト(
+      <H.A href="http://gapm.io/xiucnr4">IUCN Red List[4]</H.A>)も参考にした
+    </H.FB>
+    。
   </p>
 )
