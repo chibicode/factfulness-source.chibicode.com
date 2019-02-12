6346748a37329d6a47b8ef74c43c5a683fefe5e2
Shu Uesugi <shu@chibicode.com>
2019-02-11 23:25:24 -0800

    Update 228-asian-values.js

--- src/contents/228-asian-values.js
+++ src/contents/228-asian-values.js
@@ -4,20 +4,22 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      In &quot;Explaining Fertility Transitions&quot; (1997), Karen Oppenheim
-      Mason discusses changing family norms. Of course there are many
-      differences between cultures and how fast their values change when
-      families change to a modern lifestyle with more resources. Europe never
-      had such oppressive traditions against women like for example the foot
-      bindings of East Asia.
+      <H.FB pageNumber={360}>
+        “Explaining Fertility Transitions”(1997)で、著者のKaren Oppen-heim
+        Masonは「普通の家族」という概念が変わってきたことについて述べている。
+      </H.FB>
+      Of course there are many differences between cultures and how fast their
+      values change when families change to a modern lifestyle with more
+      resources. Europe never had such oppressive traditions against women like
+      for example the foot bindings of East Asia.
     </p>
     <p>
       Karen Oppenheim Mason discusses the changing family norms in her
       cross-disciplinary article &quot;Explaining Fertility Transitions&quot;.
-      The strength of cultural differences is easily overstated though. Gender
-      roles change quite fast in all cultures as people get richer and their way
-      of living is modernized. In cultures with an emphasis on extended
-      families, values may change a bit more slowly.
+      The strength of cultural differences is easily overstated though.
+      <H.FB pageNumber={360}>
+        所得水準が上がり生活スタイルが現代的になるにつれ、男女の役割はあっという間に変わる。しかし、親類縁者とのつながりが強い文化では、価値観の変わるスピードは遅くなる。
+      </H.FB>
     </p>
   </>
 )
