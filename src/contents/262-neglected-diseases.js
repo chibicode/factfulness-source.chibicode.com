e5313653ad36542eacc8fe811b1a0fb0d103c3d6
Shu Uesugi <shu@chibicode.com>
2019-02-12 11:13:35 -0800

    Update 262-neglected-diseases.js

--- src/contents/262-neglected-diseases.js
+++ src/contents/262-neglected-diseases.js
@@ -3,16 +3,16 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The World Health Organization has
-    <a href="http://gapm.io/xnegtrop">a list of neglected tropical diseases</a>
-     (WHO[15]) that primarily affect people living on income Level 1 and
-    markedly neglected in research and development (R&amp;D). The illnesses on
-    this list are not profitable to the pharmaceutical industry---only until
-    recently, Ebola was on this list. See also
+    <H.FB pageNumber={358}>
+      患者がレベル1に集中していて、製薬会社にとって利益にならない病気のリストは、世界保健機構のデータベース(
+      <H.A href="http://gapm.io/xnegtrop">WHO[15]</H.A>
+      )で見ることができる。エボラは最近までこのリストに載っていた。
+    </H.FB>
+    See also
     <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4306754/">
       this article by Von Philipsborn (2015)
     </a>
-     in Glob Health Action that evaluate poverty in relation to neglect in
+    in Glob Health Action that evaluate poverty in relation to neglect in
     R&amp;D.
   </p>
 )
