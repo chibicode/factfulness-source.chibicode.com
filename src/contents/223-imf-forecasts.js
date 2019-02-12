02a5a7043237e2d4440b2bed2d448f0a4c3d880b
Shu Uesugi <shu@chibicode.com>
2019-02-11 23:12:07 -0800

    Update 223-imf-forecasts.js

--- src/contents/223-imf-forecasts.js
+++ src/contents/223-imf-forecasts.js
@@ -4,15 +4,17 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      Our comments on the IMF's forecasting track record are based on the World
-      Economic Outlook; see IMF[2]. IMF forecasts are used for speculations
-      about future growth in the stock and finance industry, as
+      <H.FB pageNumber={361}>
+        国際通貨基金による過去の予測精度についてのわたしたちのコメントは、国際通貨基金による世界経済見通し(IMF[2])のデータを対象にしている
+      </H.FB>
+      IMF forecasts are used for speculations about future growth in the stock
+      and finance industry, as
       <a href="https://www.bbc.com/news/business-22228223">BBC News</a> points
       out.
       <a href="https://seekingalpha.com/article/4117923-2017-q4-stock-market-outlook-u-s-economy-reaching-peak">
         Here
       </a>
-       is one example of such speculations.
+      is one example of such speculations.
     </p>
     <p>
       See <a href="http://www.gapm.io/eecof">www.gapm.io/eecof</a>. The
@@ -30,8 +32,8 @@ export default () => (
         <a href="http://www.gapm.io/xihlex">
           IHME[1] (Institute for Health Metrics and Evaluation) Global Burden of
           Disease Study 2016 (GBD 2016) All-cause Under-5 Mortality, Adult
-          Mortality, and Life Expectancy 1970-2016. Table 13A.
-          Select Lancet article tables (2017).
+          Mortality, and Life Expectancy 1970-2016. Table 13A. Select Lancet
+          article tables (2017).
         </a>
       </li>
       <li>
