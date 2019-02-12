163105c286154baa8936eeb508b3d53a743a3635
Shu Uesugi <shu@chibicode.com>
2019-02-12 14:02:57 -0800

    Update 322-local-ignorance-and-data.js

--- src/contents/322-local-ignorance-and-data.js
+++ src/contents/322-local-ignorance-and-data.js
@@ -3,17 +3,13 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Don't miss
-    <a href="https://www.ted.com/talks/alan_smith_why_we_re_so_bad_at_statistics">
-      Alan Smith's TEDx talk &quot;Why you should love statistics&quot;
-    </a>
-    where he shows great examples of local misconceptions in the UK. Gapminder
-    is starting to develop localized visualizations, like these about Stockholm:
-    see <a href="http://www.gapm.io/gswe1">www.gapm.io/gswe1</a>. Each bubble
-    represents a small area of the city. The differences in income are
-    disturbingly large, but just like the global picture, the areas in Stockholm
-    are NOT divided into two groups: poor vs. rich, as it's usually sounds like
-    in the Swedish news. Push play and see how most of Stockholm is getting
-    richer and more educated, despite the common feeling of decline.
+    <H.FB pageNumber={354}>
+      アラン・スミスのTEDトーク
+      <H.A href="https://gapm.io/gswe1">
+        『統計を好きになるべき理由』(Alan Smith’s TEDx talk “Why you should love
+        statistics”)
+      </H.A>
+      をぜひとも見てほしい。スミスはイギリスでの勘違いの事例をあげている。ギャップマインダーでは、地域の人口動態が目でみてすぐにわかるような動くマップをつくりをはじめている。プレイをクリックすると、ストックホルムの人口ほとんどが豊かになり、教育程度が上がっていることがわかる。一方で、ストックホルムの政治議論を聞いていると事実とは逆に格差が広がっているように感じてしまう。
+    </H.FB>
   </p>
 )
