bb93406927515b37040d74effc8cbbc22768c7b3
Shu Uesugi <shu@chibicode.com>
2019-02-14 03:59:38 -0800

    forgot p tag

--- src/contents/source-acronyms.js
+++ src/contents/source-acronyms.js
@@ -3,8 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <>
-    In this list, we use the same acronyms and identifiers for sources, e.g.
-    UN-Pop[1], which are the same as you find in the Sources section at the end
-    of the printed book.
+    <p>
+      In this list, we use the same acronyms and identifiers for sources, e.g.
+      UN-Pop[1], which are the same as you find in the Sources section at the
+      end of the printed book.
+    </p>
   </>
 )
