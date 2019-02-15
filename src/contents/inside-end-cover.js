4c4f641484ae65533287e85b809ea0076e3d86f8
Shu Uesugi <shu@chibicode.com>
2019-02-14 16:20:09 -0800

    Update inside-end-cover.js

--- src/contents/inside-end-cover.js
+++ src/contents/inside-end-cover.js
@@ -4,25 +4,23 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      The chart on the pastedown at the end of the book shows people by region
-      and income. The maps show the&nbsp;
-      <H.A href="https://esa.un.org/unpd/wpp">UN-POP[1]</H.A>&nbsp;population
-      numbers from the medium fertility variant of the World Population Prospect
-      2017, rounded to full billions by&nbsp;
+      巻末の見返しには地域ごとの人口と所得ごとの暮らしを掲載している。 The chart
+      on the pastedown at the end of the book shows people by region and income.
+      The maps show the
+      <H.A href="https://esa.un.org/unpd/wpp">UN-POP[1]</H.A> population numbers
+      from the medium fertility variant of the World Population Prospect 2017,
+      rounded to full billions by
       <H.A href="http://gapm.io/elev">Gapminder[3]</H.A>. Household per capita
-      are in PPP 2011 $/day using PPP from&nbsp;
+      are in PPP 2011 $/day using PPP from
       <H.A href="http://www.gapm.io/x-icpp">ICP[1]</H.A>. Income and gini data
-      for 2013 comes from&nbsp;
+      for 2013 comes from
       <H.A href="http://iresearch.worldbank.org/PovcalNet">PovCal</H.A>,
-      extended to 2017 and 2040 by Gapminder, with a method similar to
-      what&nbsp;
+      extended to 2017 and 2040 by Gapminder, with a method similar to what
       <H.A href="http://gapm.io/xpiie17">Hellebrandt and Mauro[1]</H.A>
-      &nbsp;describe in &ldquo;The Future of Worldwide Income
-      Distribution&rdquo; from April 2015. Using&nbsp;
-      <H.A href="http://gapm.io/ximfw">IMF[1]</H.A>&nbsp;forecasts extended
-      by&nbsp;
-      <H.A href="http://gapm.io/incm">Gapminder[8]</H.A>&nbsp;all the way to
-      2040.
+      describe in &ldquo;The Future of Worldwide Income Distribution&rdquo; from
+      April 2015. Using
+      <H.A href="http://gapm.io/ximfw">IMF[1]</H.A> forecasts extended by
+      <H.A href="http://gapm.io/incm">Gapminder[8]</H.A> all the way to 2040.
     </p>
     <p>
       The log normal distributions were calculated with the method provided by
@@ -34,7 +32,7 @@ export default () => (
     <p>
       The forecasted populations in different regions on different income levels
       in 2017 and 2040 are described in more detail here. Gapminder[12], see
-      Income Mountains v. See Gapminder[12]. Read more here:&nbsp;
+      Income Mountains v. See Gapminder[12]. Read more here:
       <H.A href="https://www.gapminder.org/sources/income-mountains">
         www.gapminder.org/sources/income-mountains
       </H.A>
@@ -42,9 +40,9 @@ export default () => (
     </p>
     <ul>
       <li>
-        <strong>PovcalNet</strong>&nbsp;&ldquo;An Online Analysis Tool for
-        Global Poverty Monitoring.&rdquo; Founded by Martin Ravallion, at the
-        World Bank. Accessed November 30, 2017.&nbsp;
+        <strong>PovcalNet</strong> &ldquo;An Online Analysis Tool for Global
+        Poverty Monitoring.&rdquo; Founded by Martin Ravallion, at the World
+        Bank. Accessed November 30, 2017.
         <H.A href="http://iresearch.worldbank.org/PovcalNet">
           http://iresearch.worldbank.org/PovcalNet
         </H.A>
