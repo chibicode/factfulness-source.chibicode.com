d0b1c59f3dc390a79becff14522757cf76063e5b
Shu Uesugi <shu@chibicode.com>
2019-02-14 18:22:53 -0800

    Update 318-business.js

--- src/contents/318-business.js
+++ src/contents/318-business.js
@@ -1,34 +1,20 @@
 import React from 'react'
-import * as H from '../components/h'
 
 export default () => (
   <>
     <p>
-      The world has emerged into a continuous in which services and industries
-      move more freely. In The world is flat, Thomas L. Friedman (2005)
-      describes how most of the things we take for granted about business,
-      worklife, and the future, are no longer true. At the same time,
-      globalization has only started, as described so well by Fareed Zakaria
-      (2008), who calls it The Post-American World in his book with the same
-      title. Zakaria makes these important points:
+      世界はひとつの市場になり、サービスや製品はより自由に取引されるようになった。トーマス・フリードマンは著書『フラット化する世界』(2010年、日本経済新聞出版社)にて、ビジネスや働き方、そして未来についてのこれまでの常識が当てはまらなくなったと書いている。一方、ファリード・ザカリアは『アメリカ後の世界』(2008年、徳間書店)にて、「グローバル化はまだはじまったばかり」と説いている。彼によると、
     </p>
     <ul>
       <li>
-        Outsourcing to level 2 -- same quality for less than half the price.
-        Textiles industry Europe &gt; Bangladesh and Cambodia &gt; Kenya and
-        other African countries as Bangladesh and Cambodia become wealthier and
-        approach Level 3.
+        レベル2の国への生産移転は加速するだろう。質を落とさずにコストを半分にできる。織物産業はヨーロッパからバングラデシュやカンボジアに移った。バングラデシュやカンボジアがレベル3に近くにつれ、ケニアなどのアフリカ諸国が台頭してくるはずだ。
       </li>
       <li>
-        Ghana, Nigeria, and Kenya are some of the best investment opportunities
-        that can be found today.
+        現在、ガーナ、ナイジェリア、ケニアは投資先として最も有望な国々だ。
       </li>
     </ul>
     <p>
-      These two books opened my eyes and made me brave, because I realized that
-      others were seeing what I was seeing. Farred invited me to his studio
-      where I showed how China will catch up also, and how large parts of rural
-      China are lagging dangerously behind.
+      『フラット化する世界』と『アメリカ後の世界』にはハッとさせられた。また、わたしと同じように世界を見ている人がいることに勇気づけられた。ちなみに、『アメリカ後の世界』を書いたファリードはわたしを研究室に招待してくれた。そこでは中国がレベル4の国に追いつきつつあること、一方で中国の農村部がとても遅れていることは懸念すべきだと伝えた。
     </p>
   </>
 )
