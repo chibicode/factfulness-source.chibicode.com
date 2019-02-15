2d47e4c691cae3c82310e45841f57f37a576be13
Shu Uesugi <shu@chibicode.com>
2019-02-14 17:48:43 -0800

    Update 10-fact-question-5-future-number-of-children.js

--- src/contents/10-fact-question-5-future-number-of-children.js
+++ src/contents/10-fact-question-5-future-number-of-children.js
@@ -6,22 +6,18 @@ export default () => (
     <H.QuizQuote quizNumber={5} />
 
     <p>
-      正解はC。The UN experts (i.e. the demographers of the UN Population
-      Division) publish new official population forecasts every second year in
-      their publication 世界人口予測. They work with multiple
-      alternative scenarios. The one they think is most probable is called the
-      'medium fertility variant', which falls between the highest and lowest
-      predictions of fertility and mortality decline worldwide.{' '}
+      正解はC。国連人口部の専門家は2年おきに「世界人口予測」を発表している。いくつかの予測のなかで、最も起こりうる確実が高いのが「出生率中位」の予測だ。これは出生率と死亡率それぞれで、最高位と最低位の中間の値をとった予測だ。
+    </p>
+    <p>
       <H.FB pageNumber={372}>
         過去10年間、国連は「2100年の子供の数は現在より多くならない」という見方を貫いてきた。
       </H.FB>
-      In their latest revision published in 2017, the UN Population Division
-      estimates that there are 1.975 billion children (aged 0 to 14) in 2017 and
-      forecast that the number will be 1.957 billion in 2100 (having peaked at
-      2.094 in the year 2057); see{' '}
-      <H.A href="http://www.esa.un.org/unpd/wpp">UN-Pop[2]</H.A>. Nobody can
-      know for sure, but the question is only asking which forecast is the most
-      likely.
+      執筆時最新の「世界人口予測 2017年版(
+      <H.A href="http://www.esa.un.org/unpd/wpp">UN-Pop[2]</H.A>
+      )」によると、2017年の子供の数(0歳から14歳)は19億7500万人。この数字は2057年に20億9400万人で頭打ちになり、2100年には19億5700万人になる。
+    </p>
+    <p>
+      100%正しいとは言い切れないが、この質問の選択肢のなかでは、20億人が最も現実的なシナリオであることは間違いない。
     </p>
   </>
 )
