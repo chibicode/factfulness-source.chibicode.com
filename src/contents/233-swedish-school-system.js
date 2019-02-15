68d9e3010ded5ce7a2b38a67ee4cbdd54f076a56
Shu Uesugi <shu@chibicode.com>
2019-02-14 22:51:46 -0800

    Update 233-swedish-school-system.js

--- src/contents/233-swedish-school-system.js
+++ src/contents/233-swedish-school-system.js
@@ -3,8 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    This article explains in English what the head of OECD PISA study has to say
-    about the experiment; see The Local (2017),
+    OECDのPISA学力テストの責任者が、スウェーデンの教育改革についてどう考えているかを知るには、こちらの英語記事を読んでほしい:
+    The Local (2017),
     <H.A href="https://www.thelocal.se/20170523/deregulation-and-freedom-of-choice-have-hurt-swedens-schools-pisa-head">
       "Deregulation and freedom of choice have hurt Sweden's schools."
     </H.A>
