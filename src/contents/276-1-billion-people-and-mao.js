25ec3ae6e6f413cf6d9d554fb8fed0ae5d854490
Shu Uesugi <shu@chibicode.com>
2019-02-14 20:09:02 -0800

    Update 276-1-billion-people-and-mao.js

--- src/contents/276-1-billion-people-and-mao.js
+++ src/contents/276-1-billion-people-and-mao.js
@@ -7,8 +7,8 @@ export default () => (
       「10億人」とは、毛沢東によって人生が変わった人のおおよその人数だ。毛沢東
       が中国を支配したのは1949年から1976年。
     </H.FB>
-    The number of victims is uncertain. A list of sources and evaluation of
-    their claims can be found at
-    <H.A href="http://necrometrics.com/20c5m.htm#Mao">Necrometrics</H.A>.
+    彼の政策により亡くなった人の数を正確に知るのは難しい。
+    <H.A href="http://necrometrics.com/20c5m.htm#Mao">Necrometrics</H.A>
+    というサイトには、いくつかの死亡者数の予測と、それらがどのように算出されたかが載っている。
   </p>
 )
