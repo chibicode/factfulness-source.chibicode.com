6361ab7552d48bce911138398cb142755f875405
Shu Uesugi <shu@chibicode.com>
2019-02-14 22:15:46 -0800

    Update 288-roadblocks.js

--- src/contents/288-roadblocks.js
+++ src/contents/288-roadblocks.js
@@ -2,17 +2,16 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    Roadblocks seldom work to stop spreading an outbreak, as
-    <H.A href="https://www.nature.com/news/ebola-learn-from-the-past-1.16117">
-      David L. Heymann (2014) points out in Nature
-    </H.A>
-    about his experiences of Ebola outbreaks in the past: "Attempts to
-    block the disease at Africa's porous borders did not stop past outbreaks,
-    and will not work now. A cordon sanitaire was established by the DRC
-    government around Kikwit in 1995 and enforced through military roadblocks.
-    But contacts under fever surveillance travelled outside the cordon in dugout
-    canoes. The military patrolled roads, but not forest paths leading to the
-    Kwilu River."
-  </p>
+  <>
+    <p>
+      <H.A href="https://www.nature.com/news/ebola-learn-from-the-past-1.16117">
+        David L. Heymann (2014)
+      </H.A>
+      はネイチャー誌にて「道路封鎖は感染症の流行を止めるのに役に立たない」と語った。エボラの流行について、彼はこう書いている:
+    </p>
+    <p>
+      「アフリカでよくある脆弱な道路封鎖は、過去の感染症の流行を止めることはできなかった。今回も失敗するだろう。
+      たとえば1995年のコンゴ民主共和国では、軍が感染症を止めようとキクウィトを包囲した。しかし、感染者は丸木船で包囲網をくぐり抜けてしまった。軍は道路は封鎖できたが、クウィル川に続く森の道は封鎖できなかった。」
+    </p>
+  </>
 )
