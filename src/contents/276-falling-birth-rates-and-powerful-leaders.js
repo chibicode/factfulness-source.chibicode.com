42583d7d5d04316e6dd17e31ac13288da5df41c3
Shu Uesugi <shu@chibicode.com>
2019-02-14 20:57:11 -0800

    Update 276-falling-birth-rates-and-powerful-leaders.js

--- src/contents/276-falling-birth-rates-and-powerful-leaders.js
+++ src/contents/276-falling-birth-rates-and-powerful-leaders.js
@@ -5,30 +5,25 @@ export default () => (
   <>
     <p>
       <H.FB pageNumber={358}>
-        1949年には中国の人口は5億5000万人だった。国連人口部によると、毛沢東が中国を支配した1949年から1976年に中国では7億人が生まれた
+        1949年には中国の人口は5億5000万人だった。国連人口部によると、毛沢東が中国を支配した1949年から1976年に中国では7億人が生まれた。
       </H.FB>
-      From 1970 to 1976, the total fertility rate declined by half---when Deng
-      Xiaoping enacted China's one child policy in 1979, population growth was
-      already on the fall.
+      1970年から1976年にかけて、合計特殊出生率は半分になった。鄧小平がひとりっ子政策を実行した頃には、人口の伸びはすでにゆるやかになっていた。
     </p>
     <p>
-      Data about the population of China is derived from UN-Pop[1] and Sheng Luo
-      (1988), which reconstructed population trends in China by year. The study
-      is available
+      中国の人口データはUN-Pop[1]と中国の年次人口を再調査した
       <H.A href="https://repository.upenn.edu/dissertations/AAI8816201/">
-        here
+        Sheng Luo (1988)
       </H.A>
-      . To see the population of China move over time, click
-      <H.A href="http://www.gapm.io/vchtfr79">here</H.A> and press play.
+      を基にしている。中国の人口の変化は
+      <H.A href="http://www.gapm.io/vchtfr79">こちら</H.A>
+      からアニメーションで見れる。
     </p>
     <p>
       <H.FB pageNumber={357}>
         1800年以来すべての国で出生率が低下していることについては、
         <H.A href="https://www.gapm.io/vm4">こちらの動くチャート</H.A>
-        を見てほしい
-      </H.FB>{' '}
-      To see the population of China move over time, click
-      <H.A href="http://www.gapm.io/vchtfr79">here</H.A> and press play.
+        を見てほしい。
+      </H.FB>
     </p>
   </>
 )
