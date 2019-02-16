ae1755077174e53c905590eedc22b8b001801624
Shu Uesugi <shu@chibicode.com>
2019-02-15 23:58:59 -0800

    Update 203-comparing-stoves.js

--- src/contents/203-comparing-stoves.js
+++ src/contents/203-comparing-stoves.js
@@ -3,10 +3,9 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    See the similarities between the cooking pots in China and Nigeria, and more
-    stoves, on
     <H.A href="https://www.gapminder.org/dollar-street/matrix?thing=Stoves">
-      Dollar Street.
+      ドル・ストリート
     </H.A>
+    を見れば、中国やナイジェリア、そして他の国で使われているコンロを比べることができる。
   </p>
 )
