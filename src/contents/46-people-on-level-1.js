d0d086e4b371d882ced83fcaa033b2ee492b28f5
Shu Uesugi <shu@chibicode.com>
2019-02-16 15:45:55 -0800

    Update 46-people-on-level-1.js

--- src/contents/46-people-on-level-1.js
+++ src/contents/46-people-on-level-1.js
@@ -3,10 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Approximately 0.75 billion people live on Level 1 with less than $2 per day,
-    according to <H.A href="http://gapm.io/incm">Gapminder[8]</H.A>. The
-    threshold of $2 per day is almost identical to the World Bank's $1.9 per
-    day. Gapminder rounded that up to $2 per day to make it easier to remember,
-    and to avoid the false precision in poverty estimates that are very rough.
+    レベル1(1日2ドル以下)の暮らしをする人の数は世界に約7億5000人いる(
+    <H.A href="http://gapm.io/incm">Gapminder[8]</H.A>)
+    。世界銀行による極度の貧困の定義は「1日1.9ドル以下」だが、わたしたちは「1日2ドル以下」を使っている。2ドルのほうが覚えやすいし、1.9ドルというと、極度の貧困の調査は正確だという印象を与えてしまう。
   </p>
 )
