cefd28ada0da2417ed668e542081eb5de7ef1478
Shu Uesugi <shu@chibicode.com>
2019-02-16 15:38:47 -0800

    Update 46-children-working-in-the-household.js

--- src/contents/46-children-working-in-the-household.js
+++ src/contents/46-children-working-in-the-household.js
@@ -3,15 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    In Somalia, Ethiopia, and Rwanda, where a majority live in extreme poverty,
-    most girls aged 5 to 14 spend at least 2 hours every day doing household
-    chores like fetching water, gathering firewood and cooking. But where water
-    is far away or firewood is scarce some children spend the whole day,
-    everyday, fulfilling these tasks. Read more in the [UNICEF
-    <H.A href="http://gapm.io/xunicef2">2</H.A>] publication:
+    ソマリア、エチオピア、ルワンダなど、人口の大半が極度の貧困にある国では、5歳から14歳の女子のほとんどは毎日2時間以上家事を手伝う。水を取ってきたり、薪を拾ったり、料理を担当する。水源が遠かったり、薪が手に入りにくい地域では、1日のほとんどを家事に費やすことも少なくない。詳しくはユニセフの記事を参考に:{' '}
     <H.A href="http://gapm.io/xunicef2">
-      "Narrowing the Gaps---The Power of Investing in the Poorest
-      Children."
+      "Narrowing the Gaps---The Power of Investing in the Poorest Children."
+      (UNICEF2)
     </H.A>
   </p>
 )
