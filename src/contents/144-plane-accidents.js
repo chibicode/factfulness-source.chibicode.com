b07bc9e95f437ddd0f8eb0677757471d45672484
Shu Uesugi <shu@chibicode.com>
2019-02-16 02:03:00 -0800

    Update 144-plane-accidents.js

--- src/contents/144-plane-accidents.js
+++ src/contents/144-plane-accidents.js
@@ -7,23 +7,40 @@ export default () => (
       <H.FB pageNumber={365}>
         最近の飛行機事故の死亡者数は国際航空運送協会のデータを利用した(
         <H.A href="http://gapm.io/xiatas">IATA</H.A>
-        )。旅客機の飛行距離のデータは、飛行機事故を減らす目的でつくられた国連の
-        専門機関である、国際民間航空機関のデータを利用した(
+        )。旅客機の飛行距離のデータは、飛行機事故を減らす目的でつくられた国連の専門機関である、国際民間航空機関のデータを利用した(
         <H.A href="https://www.icao.int/sustainability/Pages/Air-Traffic-Monitor.aspx">
           ICAO[1,2,3]
         </H.A>
-        )
-      </H.FB>{' '}
-      Gapminder[16] on airplane fatalities---v1 is based on
-      <H.A href="https://www.icao.int/sustainability/Pages/Air-Traffic-Monitor.aspx">
-        ICAO[3]
-      </H.A>
-      , <H.A href="http://www.gapm.io/xbtsafat">US Air Carrier Safety Data</H.A>
-      and <H.A href="http://www.gapm.io/xbtspass">Revenue Passenger-Miles</H.A>
-      from BTS[1,2] and The Annual Reports of the U.S. Scheduled Airline
-      Industryfrom <H.A href="http://airlines.org/">ATAA</H.A>. See
-      <H.A href="http://www.gapm.io/ttranspa">www.gapm.io/ttranspa</H.A> and
-      <H.A href="http://www.gapm.io/dpland">www.gapm.io/dpland</H.A>.
+        )。
+      </H.FB>
+    </p>
+    <p>
+      飛行機事故の死亡者数の変化(
+      <H.A href="https://gapm.io/dpland">Gapminder[16]</H.A>
+      )は、以下のデータを参考にした:
+    </p>
+    <ul>
+      <li>
+        <H.A href="https://www.icao.int/sustainability/Pages/Air-Traffic-Monitor.aspx">
+          ICAO[3]
+        </H.A>
+      </li>
+      <li>
+        <H.A href="http://www.gapm.io/xbtsafat">米航空会社の安全データ</H.A>
+      </li>
+      <li>
+        <H.A href="http://www.gapm.io/xbtspass">旅客マイルのデータ</H.A>{' '}
+        (BTS[1,2] )
+      </li>
+      <li>
+        <H.A href="http://airlines.org/">米航空業界の年次報告書</H.A> (ATAA)
+      </li>
+    </ul>
+    <p>
+      さらに詳しくは
+      <H.A href="http://www.gapm.io/ttranspa">こちら</H.A>と
+      <H.A href="http://www.gapm.io/dpland">こちら</H.A>
+      をご覧いただきたい。
     </p>
 
     <H.SmallHeading>出典</H.SmallHeading>
