2e963fbcbc7da39437a209155abd809829b1008c
Shu Uesugi <shu@chibicode.com>
2019-02-16 13:24:05 -0800

    Update 15-vaccination.js

--- src/contents/15-vaccination.js
+++ src/contents/15-vaccination.js
@@ -3,10 +3,7 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Vaccination data comes from <H.A href="http://gapm.io/xwhoim">WHO[1]</H.A>.
-    Even in Afghanistan, more than 60 percent of the one-year-olds today have
-    received multiple vaccinations. None of these vaccines existed when Sweden
-    was on Level 1 or 2, which is part of the reason lives were shorter in
-    Sweden back then.
+    予防接種のデータは世界保健機関<H.A href="http://gapm.io/xwhoim">WHO[1]</H.A>
+    によるもの。アフガニスタンのような国ですら、60%以上の1歳児は複数のワクチンを接種している。それらのワクチンは、スウェーデンがレベル1や2だったころは存在しなかった。当時のスウェーデンの平均寿命が短かったのはそのためだ。
   </p>
 )
