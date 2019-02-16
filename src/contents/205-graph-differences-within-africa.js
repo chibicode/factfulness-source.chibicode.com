d85f58751e2eae538832e406084c0f06618de45b
Shu Uesugi <shu@chibicode.com>
2019-02-15 23:57:10 -0800

    Update 205-graph-differences-within-africa.js

--- src/contents/205-graph-differences-within-africa.js
+++ src/contents/205-graph-differences-within-africa.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import * as H from '../components/h'
 
 export default () => (
   <p>
-    The distance between Liberia and Kenya, using Monrovia to Mombasa as points
-    of reference, is about 3,555 miles. For London and Tehran the estimated
-    number is 2,734 miles.
+    リベリアのモンロビアからケニアのモンバサの距離は約5712km。それに比べ、イギリスのロンドンからイランのテヘランの距離は約4400kmと短い。
   </p>
 )
