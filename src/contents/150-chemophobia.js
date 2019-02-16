88eecc6f98e2c2816554c8d3f244d516302f4c54
Shu Uesugi <shu@chibicode.com>
2019-02-16 01:12:32 -0800

    Update 150-chemophobia.js

--- src/contents/150-chemophobia.js
+++ src/contents/150-chemophobia.js
@@ -2,13 +2,16 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    <H.FB pageNumber={364}>
-      化学物質への恐怖の歴史についてはGribble(2013)に詳しい。レイチェル・カーソンの『沈黙の春』からはじまり、その後の化学物質による災害で化学物質恐怖症が広がった経緯が描かれている。またGribbleは、化学物質に対する大げさで事実に基づかない恐怖が、物資のムダ使いに繋がっていると主張している。
-    </H.FB>
-    According to Gribble, "Regulatory authorities should focus not on
-    pesticides, antibiotics and dioxins, but rather on pathogens, bacteria and
-    fungi, which cause millions of cases of food-borne infections that result in
-    hospitalization or death each year."
-  </p>
+  <>
+    <p>
+      <H.FB pageNumber={364}>
+        化学物質への恐怖の歴史についてはGribble(2013)に詳しい。レイチェル・カーソンの『沈黙の春』からはじまり、その後の化学物質による災害で化学物質恐怖症が広がった経緯が描かれている。また
+        <H.A href="https://gapm.io/xfosec">Gribble</H.A>
+        は、化学物質に対する大げさで事実に基づかない恐怖が、物資のムダ使いに繋がっていると主張している。
+      </H.FB>
+    </p>
+    <p>
+      Gribbleは、「規制局は殺虫剤・抗生剤・ダイオキシンよりも、毎年何百万もの命を奪う病原菌・細菌・真菌による食中毒対策に注力すべきだ」とも書いている。
+    </p>
+  </>
 )
