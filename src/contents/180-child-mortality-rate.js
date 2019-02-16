7aed3e45ab5e3ea8c2bd52ae02a1d6ebc5933529
Shu Uesugi <shu@chibicode.com>
2019-02-15 16:56:31 -0800

    Update 180-child-mortality-rate.js

--- src/contents/180-child-mortality-rate.js
+++ src/contents/180-child-mortality-rate.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import * as H from '../components/h'
 
 export default () => (
   <p>
-    The number of infant deaths come from UN-IGME. Estimates on the number of
-    births come from UN-POP[3].
+    亡くなった赤ちゃんの数はUN-IGMEを参考にした。生まれた子の数の推定はUN-POP[3]を参考にした。
   </p>
 )
