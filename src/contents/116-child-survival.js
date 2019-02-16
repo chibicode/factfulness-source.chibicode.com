a026c193232c80c65b194773f423beefdd269629
Shu Uesugi <shu@chibicode.com>
2019-02-16 02:05:02 -0800

    Update 116-child-survival.js

--- src/contents/116-child-survival.js
+++ src/contents/116-child-survival.js
@@ -3,8 +3,7 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    See the talk at TEDxChange where Hans describes how reducing child mortality
-    is both a moral and environmental imperative, here:
-    <H.A href="http://www.gapm.io/vu5mrmoral">www.gapm.io/vu5mrmoral</H.A>.
+    ハンスはTEDxChangeで行なった講演にて、なぜ乳幼児死亡率を減らすことが倫理的にも環境保護的にも良いことなのかを語った。
+    <H.A href="http://www.gapm.io/vu5mrmoral">動画はこちらから見れる</H.A>。
   </p>
 )
