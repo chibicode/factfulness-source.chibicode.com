9de8fd517f2f63c569563018faab864289f7da2d
Shu Uesugi <shu@chibicode.com>
2019-02-16 14:29:11 -0800

    Update 15-public-awareness-of-climate-change.js

--- src/contents/15-public-awareness-of-climate-change.js
+++ src/contents/15-public-awareness-of-climate-change.js
@@ -3,11 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The first scientific hypothesis on man-made climate change due to CO2
-    emissions was published as early as 1896. The "few decades"
-    mentioned here is the time elapsed since the formation of a broad consensus
-    amongst scientist about fundamental facts in the matter, roughly in the
-    1980s, and the establishment of the Intergovernmental Panel on Climate
-    Change in 1988.
+    二酸化炭素の排出が起こす地球温暖化について、世界ではじめて科学的な仮説が立てられたのは1896年のこと。
+    一方、科学者の間で地球温暖化についての共通理解が広まったのは1980年代だ。
+    そして気候変動に関する政府間パネル(IPCC)が設立されたのは1988年。文中にある「たった数十年のうちに」というのは、「80年代から現在までの数十年のうちに」という意味だ。
   </p>
 )
