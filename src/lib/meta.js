f5baf2882ea8f2db762a17e801c6f834cca81f08
Shu Uesugi <shu@chibicode.com>
2019-02-16 12:18:24 -0800

    Better title

--- src/lib/meta.js
+++ src/lib/meta.js
@@ -1,7 +1,3 @@
-export const titleArray = [
-  '『ファクトフルネス』の',
-  'データは信頼できるのか？',
-  '脚注の翻訳4万文字'
-]
+export const titleArray = ['『ファクトフルネス』', 'ウェブ脚注の日本語版']
 
 export const description = '『ファクトフルネス』共訳者(上杉)作成'
