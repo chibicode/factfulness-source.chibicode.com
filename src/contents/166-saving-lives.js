08950ddfb61f17ee00ae6777dbdb6cbfb48c6e8f
Shu Uesugi <shu@chibicode.com>
2019-02-15 16:03:14 -0800

    Update 166-saving-lives.js

--- src/contents/166-saving-lives.js
+++ src/contents/166-saving-lives.js
@@ -6,9 +6,10 @@ export default () => (
     <H.FB pageNumber={363}>
       コストをかけずに、最も多くの命を助けることができる施策については、ユニセフを参考にした(UNICEF[2])。またユニセフは、高度な医療施設におカネを使う前に、どのような基本的な医療の仕組みをまず整備すべきか解説している。
     </H.FB>
-    The publication from UNICEF[2] is called "Narrowing the Gaps---The
-    Power of Investing in the Poorest Children" (2017), and is available
-    for PDF download here:
-    <H.A href="http://www.gapm.io/xunicef2">www.gapm.io/xunicef2</H.A>.
+    ユニセフの資料はこちら:{' '}
+    <H.A href="http://www.gapm.io/xunicef2">
+      "Narrowing the Gaps---The Power of Investing in the Poorest Children"
+      (2017)
+    </H.A>
   </p>
 )
