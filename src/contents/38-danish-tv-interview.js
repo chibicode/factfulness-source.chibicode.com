15648f26d88dbbd4cb3cd30edd1c0ce015baf53c
Shu Uesugi <shu@chibicode.com>
2019-02-16 15:24:37 -0800

    38

--- src/contents/38-danish-tv-interview.js
+++ src/contents/38-danish-tv-interview.js
@@ -3,8 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The Danish TV show is called Deadline and the interviewing journalist was
-    Adam Holm. The interview with English subtitles can be seen here:
-    <H.A href="http://gapm.io/imright">http://gapm.io/imright</H.A>.
+    わたしが出演したデンマークのテレビ番組の名前は“Deadline”。英語の字幕つきで
+    <H.A href="http://gapm.io/imright">こちらから見れる</H.A>
+    。インタビューしてくれたジャーナリストの名前はAdam Holmだ。
   </p>
 )
