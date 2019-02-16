91cff826ab86767a4b6f0e88e80cc7c076b81df4
Shu Uesugi <shu@chibicode.com>
2019-02-16 13:09:50 -0800

    Update 73-graph-catching-up-with-sweden.js

--- src/contents/73-graph-catching-up-with-sweden.js
+++ src/contents/73-graph-catching-up-with-sweden.js
@@ -5,7 +5,7 @@ export default () => (
   <p>
     <H.FB pageNumber={367}>
       世界保健チャートのアニメーション版を見てみれば、ほとんどの国がスウェーデンに追いついてきていることがわかる。また、国同士を比べることもできる。
-      <H.A href="https://www.gapminder.org/whc">アクセスはこちらから</H.A>。
+      <H.A href="https://www.gapminder.org/whc">リンクはこちら</H.A>。
     </H.FB>
   </p>
 )
