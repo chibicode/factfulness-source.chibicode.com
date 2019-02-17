1746c56c5bcb70d9a27c9cfa6530122a682861cd
Shu Uesugi <shu@chibicode.com>
2019-02-16 18:55:03 -0800

    Update 49-books-on-level-4.js

--- src/contents/49-books-on-level-4.js
+++ src/contents/49-books-on-level-4.js
@@ -4,13 +4,12 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      On Level 4, the number of new book titles published each year is roughly 1
-      per 1000 citizens. That is roughly five times higher than the average on
-      Level 3, based on
+      レベル4の国では、毎年約1000人あたり1冊の新刊が発売される。
       <H.A href="https://en.wikipedia.org/wiki/Books_published_per_country_per_year#cite_note-publishingtechnology.com-2">
         Wikipedia[1]
       </H.A>
-      divided by <H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>.
+      と<H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>
+      によれば、これはレベル3の5倍の水準だ。
     </p>
 
     <H.InlineCitation>
