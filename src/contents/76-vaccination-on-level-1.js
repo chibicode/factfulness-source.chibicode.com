d79c2f1726a91993319b178fb212aebe41e619bb
Shu Uesugi <shu@chibicode.com>
2019-02-17 15:25:26 -0800

    Update 76-vaccination-on-level-1.js

--- src/contents/76-vaccination-on-level-1.js
+++ src/contents/76-vaccination-on-level-1.js
@@ -2,19 +2,35 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    In Afghanistan in 2016, more than 60 percent of the 1-year olds have
-    received most of the vaccines as listed by
-    <H.A href="http://gapm.io/xwhoim">WHO[1]</H.A>: BCG (Tuberculosis): 74
-    percent, DTP3 (Diphtheria tetanus toxoid and pertussis): 65 percent, HepB3
-    (Hepatitis B): 65 percent, Hib3 (Haemophilus influenzae type b): 65 percent,
-    MCV1 (Measles, 1st dose): 62 percent, PAB (Neonatal tetanus): 65 percent,
-    PCV3 (Pneumococcal conjugate): 65 percent and Pol3 (Polio): 60 percent. Only
-    MCV2 (the second shot against measles which brings immunity from 95 to 99.99
-    percent) is lower, at 39 percent, which is the case in many countries on
-    Level 1.{' '}
-    <H.FB pageNumber={367}>
-      これらのワクチンは、スウェーデンがレベル1や2だった頃には開発されていなかった。これは、当時のスウェーデンの平均寿命がいまのアフガニスタンより短かった理由のひとつだ。
-    </H.FB>
-  </p>
+  <>
+    <p>
+      2016年のアフガニスタンでは、1歳児の60%以上が世界保健機関が推奨する予防接種を受けている。接種率は以下の通り。ソースは
+      <H.A href="http://gapm.io/xwhoim">WHO[1]</H.A>。
+    </p>
+    <ul>
+      <li>BCGワクチン(結核): 74%</li>
+      <li>三種混合ワクチン(ジフテリア、百日咳、破傷風): 65%</li>
+      <li>B型肝炎ワクチン: 65%</li>
+      <li>ヘモフィルスインフルエンザ菌B型ワクチン: 65%</li>
+      <li>麻疹ワクチンの初回接種: 62%</li>
+      <li>破傷風ワクチン: 65%</li>
+      <li>肺炎球菌ワクチン: 65%</li>
+      <li>ポリオワクチン: 60%</li>
+    </ul>
+    <p>
+      レベル1の多くの国と同じく、麻疹ワクチンの2回目の接種率のみ60%を下回り、39%だった。ちなみにこれは麻疹の抗体の保有率を95%から99.99%に上げるワクチンだ。
+    </p>
+    <p>
+      <H.FB pageNumber={367}>
+        これらのワクチンは、スウェーデンがレベル1や2だった頃には開発されていなかった。これは、当時のスウェーデンの平均寿命がいまのアフガニスタンより短かった理由のひとつだ。
+        <H.Addition>2016年のアフガニスタンの平均寿命は約63歳。</H.Addition>
+      </H.FB>
+    </p>
+    <H.BlockAddition>
+      <p>
+        <H.BlockAdditionHeader />
+        アフガニスタンは、2019/2/17時点で外務省が国全体に退避勧告を出している7カ国に含まれている。その7カ国とはリビア、南スーダン、中央アフリカ共和国、ソマリア、イエメン、シリア、アフガニスタンのみ。そんな国でも、1歳児の60%以上が予防接種を受けている。
+      </p>
+    </H.BlockAddition>
+  </>
 )
