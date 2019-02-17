defef1a6e4ab7e6ad37677b1b7fd8c190f9f667d
Shu Uesugi <shu@chibicode.com>
2019-02-16 19:57:00 -0800

    Update 54-in-apartheid-south-africa.js

--- src/contents/54-in-apartheid-south-africa.js
+++ src/contents/54-in-apartheid-south-africa.js
@@ -4,14 +4,10 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      At present, the average white household in South Africa can spend roughly
-      five times more money than the average black household. During apartheid
-      in the 1970s, white people earned on average 12 times more than black
-      people, according data from IRR. In the United States as of 2016, white
-      households earned on average 1.6 times more than black households, $65,041
-      and $39,490 respectively; see US-CPS below. In this
-      <H.A href="http://www.gapm.io/xsa">article</H.A>, BBC reports on historic
-      race specific employment and incomes in South Africa, using data from IRR.
+      現在、南アフリカの平均的な白人家族は、平均的な黒人家族の5倍のおカネを使っている。IRRによると、1970年代のアパルトヘイト時代では、白人の平均所得はは黒人の約12倍だった。US-CPSによると、2016年のアメリカでは、平均的な白人家族の所得は黒人家族の約1.6倍だった(白人:
+      6万5041ドル、黒人: 3万9490ドル)。 また、
+      <H.A href="http://www.gapm.io/xsa">こちらのBBCの記事では</H.A>
+      、南アフリカにおいて人種が雇用と所得にどう影響してきたかをIRRのデータを用いて紹介している。
     </p>
     <H.InlineCitation>
       <ul>
