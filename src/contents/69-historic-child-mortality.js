59e04ec8cd073bac1458d807dbf3e823777b202a
Shu Uesugi <shu@chibicode.com>
2019-02-17 14:07:07 -0800

    Update 69-historic-child-mortality.js

--- src/contents/69-historic-child-mortality.js
+++ src/contents/69-historic-child-mortality.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import * as H from '../components/h'
 
 export default () => (
   <p>
-    As populations didn't grow much before 1800 despite the large number of
-    babies per woman, child mortality (death rate before age 5) must have been
-    almost 50 percent on average throughout human history, and then almost half
-    of those surviving their first five years, died before finishing their
-    parenthood. Livi-Bacci (1989, p. 17) suggests that 55 percent of children
-    died when life expectancy was 20 years, and 40 percent died when life
-    expectancy was 50 years.
+    1800年以前、女性ひとりあたりの子供の数は多かったものの、人口はあまり増えなかった。ということは、人類史の大半で、乳幼児死亡率(5歳までに亡くなる確率)は50%以上だったに違いない。そして無事5歳になったとしても、おそらく半分近くが大人になるまでに亡くなっていたのだろう。リヴィ-バッチ著『人口の世界史』(2014年、速水融・斎藤修訳、東洋経済新報社)によると、平均寿命が20歳のころは子供の55%が亡くなり、平均寿命が40歳のころは子供の40%が亡くなっていた。
   </p>
 )
