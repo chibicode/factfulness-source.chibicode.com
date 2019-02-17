5be20a52fe31bbab7dac12448a31024a9093f995
Shu Uesugi <shu@chibicode.com>
2019-02-16 19:25:04 -0800

    Update 49-travel-and-vacation-on-level-4.js

--- src/contents/49-travel-and-vacation-on-level-4.js
+++ src/contents/49-travel-and-vacation-on-level-4.js
@@ -3,16 +3,13 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    More than half the citizens in countries on Level 4 traveled abroad in 2014,
-    and large amount of of those trips were for leisure; see
+    トリップアドバイザー社は、Ipsos
+    MORIと共同で2015年1月16日から2月2日のあいだに観光に関するオンライン調査を行なった。報告書(
     <H.A href="https://www.tripadvisor.com/TripAdvisorInsights/wp-content/uploads/2018/01/TripBarometer-March2015-Global-Report-US.pdf">
       The TripBarometer 2015
     </H.A>
-    , by TripAdvisor, based on an online survey conducted from January 16 to
-    February 2 in 2015 by Ipsos MORI. On Level 4, the average number of tourist
-    departures abroad in 2015 was roughly 600 per 1,000 citizens, which is 6
-    times higher than the average for Level 3, based on
-    <H.A href="http://gapm.io/xwb1713">World Bank[13]</H.A> and
-    <H.A href="https://esa.un.org/unpd/wpp">UN-POP[1]</H.A>.
+    )によると、2014年にはレベル4の国の人口の半分以上(1000人中約600人)が2014年に国外に旅行した。大半は観光目的だった。これはレベル3と比べると人口あたり6倍以上の水準だ。(参考:{' '}
+    <H.A href="http://gapm.io/xwb1713">World Bank[13]</H.A>、
+    <H.A href="https://esa.un.org/unpd/wpp">UN-POP[1]</H.A>)
   </p>
 )
