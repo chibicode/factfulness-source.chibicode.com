b88450f4ff76974b78ef6dc568c41b0a06afdc86
Shu Uesugi <shu@chibicode.com>
2019-02-17 14:31:15 -0800

    Update 70-deaths-from-starvation-in-ethiopia.js

--- src/contents/70-deaths-from-starvation-in-ethiopia.js
+++ src/contents/70-deaths-from-starvation-in-ethiopia.js
@@ -4,11 +4,10 @@ import * as H from '../components/h'
 export default () => (
   <p>
     <H.FB pageNumber={368}>
-      掲載した数字はFRDとEM-DATがそれぞれ公表している数字を平均したもの。
+      掲載した数字は<H.A href="https://gapm.io/xfdi">FRD</H.A>と
+      <H.A href="https://www.emdat.be">EM-DAT</H.A>
+      がそれぞれ公表している数字を平均したもの。
     </H.FB>
-    FRD, page 87 says: "By the end of 1973, famine had claimed the lives of
-    about 300,000 peasants of Tigray and Welo, and thousands more had sought
-    relief in Ethiopian towns and villages." EM-DAT has a record of 100,000
-    deaths from the famine in Ethiopia in 1973.
+    FRD(p87)には、「1973年が終わるころには、ティグレイとウェロに暮らす30万人の農民が飢餓で亡くなり、エチオピア中で多くの人が救援に頼らざるを得なくなった」と書かれている。EM-DATは、1973年にエチオピアで10万人が亡くなったとしている。
   </p>
 )
