bf28419ae94bd0568c908ce14bfb08751cac83ab
Shu Uesugi <shu@chibicode.com>
2019-02-16 21:56:18 -0800

    57 and 58

--- src/contents/57-poverty-and-extreme-poverty.js
+++ src/contents/57-poverty-and-extreme-poverty.js
@@ -2,21 +2,28 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    <H.FB pageNumber={369}>
-      「極度の貧困(extreme
-      poverty)」は専門用語で、収入が1日1.9ドル以下である状態を示す。多くのレベル4の国では、「貧困」は相対的に定義される。「貧困ライン」は国が明確に定義したものだったり、社会保障サービスを受けられる基準だったりする。北欧諸国の公式の貧困ラインは、マラウィなど最も貧しい国の貧困ラインと比べると、購買力の大きな差を考慮したとしても20倍の差がある(
-      <H.A href="http://gapm.io/xwb1717">World Bank[17]</H.A>
-      )。アメリカの最新の国勢調査によれば、人口の13%は収入が貧困ラインを下回っている。その貧困ラインとは1日約20ドルの収入を指す。
-    </H.FB>
-    In Sweden, the official numbers of "poor" are defined relative to
-    the median income of the country, by counting all individuals with incomes
-    less than 60 percent of the median income.
-    <H.FB pageNumber={368}>
-      豊かな国の最も貧しい人たちが直面する社会的・経済的な苦難を軽んじるべきではない(
-      <H.A href="http://gapm.io/xwb175">World Bank[5]</H.A>
-      )。しかし、それと「極度の貧困」は違う。
-    </H.FB>
-    極度の貧困にある人は、毎日のポリッジ(粥)すら買えないこともある。少しでもおカネが足りなくなったら死あるのみだ。
-  </p>
+  <>
+    <p>
+      <H.FB pageNumber={369}>
+        「極度の貧困(extreme
+        poverty)」は専門用語で、収入が1日1.9ドル以下である状態を示す。
+      </H.FB>
+    </p>
+    <p>
+      <H.FB pageNumber={369}>
+        多くのレベル4の国では、「貧困」は相対的に定義される。「貧困ライン」は国が明確に定義したものだったり、社会保障サービスを受けられる基準だったりする。北欧諸国の公式の貧困ラインは、マラウィなど最も貧しい国の貧困ラインと比べると、購買力の大きな差を考慮したとしても20倍の差がある(
+        <H.A href="http://gapm.io/xwb1717">World Bank[17]</H.A>
+        )。アメリカの最新の国勢調査によれば、人口の13%は収入が貧困ラインを下回っている。その貧困ラインとは1日約20ドルの収入を指す。
+      </H.FB>
+      スウェーデンだと、国が定める「貧困層」は、所得が中央値の6割以下の層のことを指す。
+    </p>
+    <p>
+      <H.FB pageNumber={368}>
+        豊かな国の最も貧しい人たちが直面する社会的・経済的な苦難を軽んじるべきではない(
+        <H.A href="http://gapm.io/xwb175">World Bank[5]</H.A>
+        )。しかし、それと「極度の貧困」は違う。
+      </H.FB>
+      極度の貧困にある人は、毎日のポリッジ(粥)すら買えないこともある。少しでもおカネが足りなくなったら死あるのみだ。
+    </p>
+  </>
 )
--- src/contents/58-levels-of-poverty.js
+++ src/contents/58-levels-of-poverty.js
@@ -3,10 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Voices of the Poor is a three volume publication from
-    <H.A href="http://gapm.io/xwb1712">World Bank[12] </H.A>of interviews with
-    poor people across 47 countries. The interviews shed light on the existing
-    differences between levels of poverty. It is clear that those who live in
-    poverty are themselves well-aware of these differences.
+    世界銀行は
+    <H.A href="http://gapm.io/xwb1712">
+      Voices of the Poor (World Bank [12])
+    </H.A>
+    という全3巻からなるインタビュー集を出している。47カ国の貧しい人々に話を聞いており、これを読めば貧しさの度合いは千差万別であることが分かる。そして貧しい人たちは、貧しさの度合いに違いがあることを身をもって感じている。
   </p>
 )
