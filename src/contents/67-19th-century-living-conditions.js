0697bba9b8e16117bd9f2e520c1f517151f97a97
Shu Uesugi <shu@chibicode.com>
2019-02-17 13:55:24 -0800

    Update 67-19th-century-living-conditions.js

--- src/contents/67-19th-century-living-conditions.js
+++ src/contents/67-19th-century-living-conditions.js
@@ -2,19 +2,17 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    United Kingdom probably had among the world's longest lives in 1800, on
-    average 36 years according to Livi-Bacci (1989), A Concise History of World
-    Population. Swedes and almost everyone else lived to 32 years or less; see
-    <H.A href="http://gapm.io/ilex">Gapminder[4]</H.A>. British children began
-    to work at an early age, on average at 10, which varied between regions.
-    Young children were extremely valuable in British coal mines because they
-    were small. Until 1842, many children, some as young as five years old, died
-    while working their 10 hour shifts as trappers. For more on child labor in
-    industrial England, see
-    <H.A href="https://www.bl.uk/romantics-and-victorians/articles/child-labour">
-      this article
-    </H.A>
-    by Griffin (2014).
-  </p>
+  <>
+    <p>
+      1800年に最も平均寿命が長かったのは、おそらくイギリスだろう。リヴィ-バッチ著『人口の世界史』(2014年、速水融・斎藤修訳、東洋経済新報社)によると、当時のイギリスの平均寿命は36歳だった。スウェーデンや他の国では平均寿命は32歳以下だった(
+      <H.A href="http://gapm.io/ilex">Gapminder[4]</H.A>)。
+    </p>
+    <p>
+      イギリスの子どもたちは小さいうちから仕事をさせられた。地域ごとに違うが、みんなだいたい10歳で仕事についた。身体が小さい子どもたちは狭い炭鉱では重宝された。1842年に炭鉱での児童労働を禁止する法律が通るまで、多くの子どもたちが長時間労働で亡くなった。5歳で亡くなった子どももいた。産業革命下のイギリスでの児童労働について詳しくは
+      <H.A href="https://www.bl.uk/romantics-and-victorians/articles/child-labour">
+        こちらの記事(Griffin (2014))を参考に
+      </H.A>
+      。
+    </p>
+  </>
 )
