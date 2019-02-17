11a919264fb99412c966e80e67fde77087d3bf0c
Shu Uesugi <shu@chibicode.com>
2019-02-17 02:05:52 -0800

    Update meta.js

--- src/lib/meta.js
+++ src/lib/meta.js
@@ -1,3 +1,7 @@
-export const titleArray = ['『ファクトフルネス』', 'ウェブ脚注の日本語版']
+export const titleArray = [
+  '『ファクトフルネス』',
+  'ウェブ脚注の日本語版',
+  '○万文字'
+]
 
 export const description = '『ファクトフルネス』共訳者(上杉)作成'
