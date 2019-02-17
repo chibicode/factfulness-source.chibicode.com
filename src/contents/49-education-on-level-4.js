598b7713d428a789441785781d5127ad923f6349
Shu Uesugi <shu@chibicode.com>
2019-02-16 19:01:42 -0800

    Update 49-education-on-level-4.js

--- src/contents/49-education-on-level-4.js
+++ src/contents/49-education-on-level-4.js
@@ -3,11 +3,8 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The average length of schooling is above 12 years in all countries on Level
-    4, including many of the gulf states that only recently reached Level 4,
-    like Saudi Arabia and the UAE. The only exceptions on Level 4 are the three
-    small gulf states: Bahrain, Oman, and Qatar. See
-    <H.A href="http://gapm.io/xihedu">IHME[2]</H.A>and
-    <H.A href="http://www.gapm.io/xbl17">Barro-Lee</H.A>.
+    レベル4にいるほとんどの国において、人々は平均12年以上教育を受ける。これはつい最近レベル4になったサウジアラビアやアラブ首長国連邦にも当てはまる。例外はバーレーン、オマーン、カタールなど、比較的小さい湾岸諸国のみだ。詳しくは
+    <H.A href="http://gapm.io/xihedu">IHME[2]</H.A>と
+    <H.A href="http://www.gapm.io/xbl17">Barro-Lee</H.A>を参照のこと。
   </p>
 )
