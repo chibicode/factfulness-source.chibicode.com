94d1215d724263c9318e63107a0a4f069165a170
Shu Uesugi <shu@chibicode.com>
2019-02-18 11:58:39 -0800

    Update 303-the-risk-of-financial-collapse.js

--- src/contents/303-the-risk-of-financial-collapse.js
+++ src/contents/303-the-risk-of-financial-collapse.js
@@ -2,21 +2,19 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    <H.FB pageNumber={355}>
-      過去10年間は、外部環境の変動が激しく、特に資本市場は極端な出来事による変動にさらされてきたとリチャード・ドブスらはNo
-      Ordinary Disruption(2016)にて書いている。
-    </H.FB>
-    illustrated by the peaks in the trend line on page 88. In his article
-    "How Should We Prevent the Next Financial Crisis?" (2015) Ricardo
-    Hausmann suggests that since the financial system constantly changes, it is
-    difficult to learn from past mistakes---our next financial crisis will
-    simply not look like any other that has happened before. Many countries have
-    regulated their financial systems to prevent future crises by restricting
-    loans and risk-taking. While this strategy is good for some countries,
-    preventative measures do not benefit all. A number of countries in Latin
-    America for example, Hausmann writes, have instead made their economies too
-    secure. You can read the article by Hausmann in full at
-    <H.A href="http://www.gapm.io/xecc">www.gapm.io/xecc</H.A>.
-  </p>
+  <>
+    <p>
+      <H.FB pageNumber={355}>
+        過去10年間は、外部環境の変動が激しく、特に資本市場は極端な出来事による変動にさらされてきたとリチャード・ドブスらはNo
+        Ordinary Disruption(2016 p88)にて書いている。
+      </H.FB>
+    </p>
+    <p>
+      Ricardo Hausmannの
+      <H.A href="https://gapm.io/xecc">
+        "How Should We Prevent the Next Financial Crisis?" (2015)
+      </H.A>
+      という記事によると、金融の仕組みは常に変わり続けているので、過去の間違いから学ぶことが難しい。将来の金融危機は、過去の金融危機とは似ても似つかないものになる可能性がある。多くの国は、危険な融資や投資を規制している。だが、金融規制は国によっては良い手にも悪い手にもなる。Hausmannによると、ラテンアメリカのいくつかの国では、金融規制のしすぎで経済ががんじがらめになっている。
+    </p>
+  </>
 )
