cfc71fe7ba9c64ce99637f5844f4d6f87e9ac0dc
Shu Uesugi <shu@chibicode.com>
2019-02-17 20:48:06 -0800

    Update 123-e-coli-bacteria.js

--- src/contents/123-e-coli-bacteria.js
+++ src/contents/123-e-coli-bacteria.js
@@ -3,10 +3,7 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The generation time of E.coli bacteria, or the time required for the
-    bacteria to double in number, is 15--20 minutes in a laboratory "but in
-    the intestinal tract, the coliform's generation time is estimated to be
-    12-24 hours," as described in this bacteriology textbook:
-    <H.A href="http://www.gapm.io/xecol">gapm.io/xecol</H.A>.
+    大腸菌の世代時間(倍増にかかる時間)は、実験室では15〜20分だが、胃腸管では約12時間から24時間かかる。ソースは
+    <H.A href="http://www.gapm.io/xecol">こちら</H.A>。
   </p>
 )
