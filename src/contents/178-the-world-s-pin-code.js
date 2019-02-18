a849a957f3c0020cf739815ff35c8441ec1e5897
Shu Uesugi <shu@chibicode.com>
2019-02-17 21:34:18 -0800

    Update 178-the-world-s-pin-code.js

--- src/contents/178-the-world-s-pin-code.js
+++ src/contents/178-the-world-s-pin-code.js
@@ -2,16 +2,12 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    These maps show the official UN forecasts, rounded to billions, from the
-    <H.A href="https://esa.un.org/unpd/wpp/">2017 Revision of 世界人口予測</H.A>
-    from <H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>. The world
-    population was 7.55 billion in 2017 according to their estimates. That would
-    usually be rounded to 8 billion, but we show 7 billion because we are
-    rounding the population region by region. The populations of the{' '}
-    <H.A href="http://gapm.io/ireg">four Gapminder regions</H.A>
-    were estimated based on national data: the Americas, 1.0 billion; Europe,
-    0.84 billion; Africa, 1.3 billion; and Asia, 4.4 billion. See also the note
-    to Fact Question 8.
-  </p>
+  <>
+    <p>
+      <H.OtherCardLink permalink="11-fact-question-8-where-people-live">
+        こちらの脚注を参考に
+      </H.OtherCardLink>
+      。
+    </p>
+  </>
 )
