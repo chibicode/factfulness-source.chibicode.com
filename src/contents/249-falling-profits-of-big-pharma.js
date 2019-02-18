ad7925f43c56ba2c3f3019b704ca8ac08b982581
Shu Uesugi <shu@chibicode.com>
2019-02-18 00:11:33 -0800

    Update 249-falling-profits-of-big-pharma.js

--- src/contents/249-falling-profits-of-big-pharma.js
+++ src/contents/249-falling-profits-of-big-pharma.js
@@ -3,19 +3,13 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    In
     <H.A href="https://doi.org/10.1377/hlthaff.2014.1029">
-      a study published in Health Affairs
+      Health Affairsに掲載されたBerndt et al. (2015)の論文
     </H.A>
-    , Berndt et al. (2015) analyzed the falling profits of Big Pharma,
-    concluding that: "If this level of diminished returns persists, we
-    believe that the rewards for innovation will not be sufficient for
-    pharmaceutical manufacturers to maintain the historical rates of investments
-    needed to sustain biomedical innovation." Forbes (2016) refers to this
-    study, discussing the presumed end of the so-called
+    によると、大手製薬会社の利益は減り続けている。また、このまま利益が減り続ければ、製薬のイノベーションに必要な投資を続けられなくなるという。フォーブス誌は2016年にこの論文を取りあげ、「
     <H.A href="https://www.forbes.com/sites/peterubel/2016/07/29/is-the-golden-era-of-pharmaceutical-profits-over/#9e5d4937207b">
-      Golden Era of pharmaceutical profits
+      製薬会社ががっぽり儲かっていた時代は終わった
     </H.A>
-    .
+    」と書いている。
   </p>
 )
