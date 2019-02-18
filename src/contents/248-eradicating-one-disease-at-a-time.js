7a5251e643e961377532b9dd2b85fefc1fe8b77d
Shu Uesugi <shu@chibicode.com>
2019-02-18 00:48:53 -0800

    Update 248-eradicating-one-disease-at-a-time.js

--- src/contents/248-eradicating-one-disease-at-a-time.js
+++ src/contents/248-eradicating-one-disease-at-a-time.js
@@ -3,14 +3,14 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Halfdan Mahler, the Director-General of WHO from 1973 to 1988, describes in
+    1973年から1988年まで世界保健機関の事務局長を務めたハルフダン・マーラーは、病気をひとつずつ根絶するのではなく、総合的な医療を優先すべきだという結論を出した理由について
     <H.A href="http://www.who.int/bulletin/volumes/86/10/08-041008/en/">
-      an interview from 2008
+      2008年のインタビューで
     </H.A>
-    how they came to promote primary health care to all instead of eradicating
-    one disease at a time. It was declared "a most important world-wide
-    social goal" at the International Conference on Primary Health Care in
-    Alma Ata 1978. The publication is available to download as PDF:
-    <H.A href="http://www.gapm.io/xalmaata">www.gapm.io/xalmaata</H.A>.
+    説明している。また、1978年にアルマ・アタで行われた第一回プライマリ・ヘルス・ケアに関する国際会議にて、「
+    <H.A href="http://www.gapm.io/xalmaata">
+      総合的な医療の充実こそ、世界中で最優先目的とすべき
+    </H.A>
+    」という宣言が行われた。
   </p>
 )
