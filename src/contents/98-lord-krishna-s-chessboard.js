f71b07319b34c7f317847d5cb05abe9c15a55b3c
Shu Uesugi <shu@chibicode.com>
2019-02-17 17:14:03 -0800

    Update 98-lord-krishna-s-chessboard.js

--- src/contents/98-lord-krishna-s-chessboard.js
+++ src/contents/98-lord-krishna-s-chessboard.js
@@ -2,17 +2,19 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    The Indian legend, depicting the effect of doubling, is called the
-    <H.A href="https://en.wikipedia.org/wiki/Ambalappuzha_Sri_Krishna_Temple#Legend_of_the_Ambalappuzha_Paal_Payasam">
-      Legend of the Ambalappuzha Paal Payasam
-    </H.A>
-    , named after the temple where it supposedly happened. In calculating the
-    volume of rice to cover India, the total number of grains Lord Krishna is
-    owed were divided by India's total land area. The grains' estimated weight
-    and volume---300 grains of rice equals 6 grams (thanks,
-    <H.A href="http://www.gapm.io/xrice">gapm.io/xrice</H.A>)---were converted
-    into density per square meter, using this grain converter:
-    <H.A href="http://www.gapm.io/xcalc">gapm.io/xcalc</H.A>.
-  </p>
+  <>
+    <p>
+      倍々ゲームの怖さを教えてくれるインドの昔話は、「
+      <H.A href="https://en.wikipedia.org/wiki/Ambalappuzha_Sri_Krishna_Temple#Legend_of_the_Ambalappuzha_Paal_Payasam">
+        アンバラプーザ・パール・ペヤサムの伝説
+      </H.A>
+      」と呼ばれている。由来はこの話があった寺院の名前だ。
+    </p>
+    <p>
+      インド全土を覆うのに必要な米の量は次のように計算した。まず、
+      <H.A href="http://www.gapm.io/xrice">米は約300粒で6グラム</H.A>になる。
+      次に<H.A href="http://www.gapm.io/xcalc">こちらのツール</H.A>
+      を使い、1平方メートルあたりの密度を計算した。最後に、出てきた数字をインドの国土の大きさで割った。
+    </p>
+  </>
 )
