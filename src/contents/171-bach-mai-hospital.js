0c652252d6e31c4a71f956b0f1757142f3e3ca1b
Shu Uesugi <shu@chibicode.com>
2019-02-17 20:52:16 -0800

    Update 171-bach-mai-hospital.js

--- src/contents/171-bach-mai-hospital.js
+++ src/contents/171-bach-mai-hospital.js
@@ -3,11 +3,11 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    The bombing of the Bach Mai hospital in Hanoi, Vietnam, in 1972 and the
-    various reports of events are described in more detail
+    1972年にベトナム・ハノイで起きたバックマイ病院の爆撃についてはWilliam
+    Stewart Logan著“Hanoi: Biography of a City”にある
     <H.A href="https://books.google.se/books?id=QiYG5lXd4XsC&amp;pg=PA173&amp;lpg=PA173&amp;dq=bach+main+bombs+killed&amp;source=bl&amp;ots=ndDRbaFxwO&amp;sig=blxeTu16nDfD9xt7zpKo3XdW9gc&amp;hl=en&amp;sa=X&amp;ved=0ahUKEwikwoXr5oLYAhWKK5oKHaNoD2cQ6AEIRjAF#v=onepage&amp;q=bach%20main%20bombs%20killed&amp;f=false">
-      in this passage
+      こちらの一文
     </H.A>
-    in the book Hanoi: Biography of a City by William Stewart Logan.
+    を参考に。
   </p>
 )
