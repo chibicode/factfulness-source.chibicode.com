6b127eb7675eccc5ca8713a71cb2b5b772ae5feb
Shu Uesugi <shu@chibicode.com>
2019-02-18 00:01:28 -0800

    Update 254-chart-cuba.js

--- src/contents/254-chart-cuba.js
+++ src/contents/254-chart-cuba.js
@@ -4,20 +4,11 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      Hans was showing child survival rates on his bubble charts back then. We
-      later decided to show life expectancy so that people wouldn't think we
-      were focusing only on children. Life expectancy and child survival go hand
-      in hand.
+      当時、ハンスはキューバの人たちに乳幼児生存率のバブルチャートを見せていた。だが、本書では平均寿命に差し替えている。「子供だけが健康なのかもしれない」という批判を防ぐためだ。本当は、平均寿命と乳幼児生存率の両方を考慮するのが望ましい。
     </p>
+    <p>また紙面の都合上、最も金持ちの国々と、最も不健康な国々は省いている。</p>
     <p>
-      The charts hide a few richer countries to save space to the right, and the
-      most unhealthy countries, at the bottom.
-    </p>
-    <p>
-      Next to Cuba, the new data puts Costa Rica and Panama on the same income,
-      but with higher life expectancy. That wasn't the case when Hans was
-      showing the graph in Cuba. The Cuban experience is described at greater
-      length in Hans's memoirs; see Rosling[8].
+      現在、コスタリカとパナマはキューバと同じ所得だが、平均寿命は高い。ただ、ハンスがキューバにいたころはそうではなかった。
     </p>
   </>
 )
