5fde02660c310d4b474a1b42d6ac62922be5dc5c
Shu Uesugi <shu@chibicode.com>
2019-02-17 21:38:31 -0800

    Update 268-gapminder-s-ignorance-project.js

--- src/contents/268-gapminder-s-ignorance-project.js
+++ src/contents/268-gapminder-s-ignorance-project.js
@@ -3,15 +3,12 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    In 2013, BBC and CNN reported on the results from Gapminder's Ignorance
-    Project and posted our fact questions on their websites. Here are the
+    2013年に、BBCとCNNがギャップマインダーのチンパンジークイズの結果を報道した。どちらの局もクイズを自社のウェブサイトで受けられるようにした。
+    CNN(アメリカ)の結果は
     <H.A href="https://edition.cnn.com/2013/12/10/world/gapminder-us-ignorance-survey/index.html">
-      US results on CNN
+      こちら
     </H.A>
-    and
-    <H.A href="https://www.bbc.com/news/magazine-24836917">
-      the British results on BBC
-    </H.A>
-    (2013).
+    。 BBC(イギリス)の結果は
+    <H.A href="https://www.bbc.com/news/magazine-24836917">こちら</H.A>。
   </p>
 )
