bafa1a0f2327d7e24de148803a4eae0b2710d6bf
Shu Uesugi <shu@chibicode.com>
2019-02-17 17:48:59 -0800

    Update 100-the-world-population-is-not-just-increasing.js

--- src/contents/100-the-world-population-is-not-just-increasing.js
+++ src/contents/100-the-world-population-is-not-just-increasing.js
@@ -3,10 +3,10 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Roughly a billion people will be added over the next 13 years, based on the
-    2017 revision of 世界人口予測 from the population indicator medium fertility
-    variant; see UN Population Data below,{' '}
-    <H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>. The data is freely
-    available <H.A href="http://www.esa.un.org/unpd/wpp">online</H.A>.
+    国連による
+    <H.A href="https://esa.un.org/unpd/wpp">
+      世界人口予測(2017年版、中位予測)
+    </H.A>
+    によれば、次の13年で世界人口は約10億人増える。
   </p>
 )
