b5ecdad6c28b076367402ae283ee8c03953ead63
Shu Uesugi <shu@chibicode.com>
2019-02-18 11:11:42 -0800

    Update 297-climate-reductionism-and-catastrophizing.js

--- src/contents/297-climate-reductionism-and-catastrophizing.js
+++ src/contents/297-climate-reductionism-and-catastrophizing.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import * as H from '../components/h'
 
-// Ruth DeFries 著 The Big Ratchet(2014)
+//
 export default () => (
   <>
     <p>
       <H.FB pageNumber={356}>
         すべての問題の責任を温暖化というひとつの原因になすりつけるのは悪い癖で、これが「温暖化責任論」と呼ばれるものだ。ただし、地球温暖化を否定しても問題は解決できない。これまでも歴史の中で人々が新しい環境に適応してきた多くの事例を念頭に置きながら、これから人間が温暖化とどのように共生していくかを現実的に予測したほうがいい。
       </H.FB>
-      Climate change is among other problems, and it is there are many examples
-      in world history of humans adapting to new circumstances.
+      地球温暖化は、他の課題と同じように考えるべきだ。
     </p>
     <p>
-      The idea of climate change as a single scenario is refuted by Smil (2008).
-      In his book Global Catastrophes and Trends, Smil uses global warming as a
-      good example of a process that is too complex to happen as a single event.
-      An assumption that is questioned by Johan Rockström and others who insist
-      that this line of thinking may be a case of the straight line instinct.
-      Smil argues that climate change will probably not lead to a catastrophic
-      event destroying everything at ones, but neither will it be a slow
-      transition that we won't notice. The future effects of climate change will
-      have aspect of both fast and gradual transition. Fascinating examples of
-      pivotal moments in human history, in regards to food and energy
-      consumption, is also given by Ruth DeFries (2014). In The Big Ratchet,
-      DeFries convincingly shows how the current human-produced system is
-      unsustainable, with fertilizers and depletion of natural resources,
-      without claiming that this must necessarily lead to a sudden catastrophic
-      event.
+      Smilは著作
+      <H.A href="http://gapm.io/xsmilcat">
+        “Global Catastrophes and Trends” (2008)
+      </H.A>
+      にて、「地球温暖化は複雑すぎて、たったひとつだけの結果をもたらすとは言えない」と語っている。彼によると、地球温暖化がひとつの巨大な災害を起こす可能性は少ないが、ゆっくりとして誰も気づかない変化をもたらす可能性も少ない。地球温暖化がもたらす変化は、「早い」と「ゆっくり」の中間になる可能性が高い。
+    </p>
+    <p>
+      地球温暖化以外にも、食料やエネルギーでも同じことが言える。Ruth
+      DeFriesは著作
+      <H.A href="http://www.ruthdefries.com/book/big-ratchet/">
+        “The Big Ratchet(2014)”
+      </H.A>
+      にて、人類史における食料とエネルギー消費の転換点について描いている。DeFriesは、化学肥料を使い続け、自然を破壊し続けるのは持続的ではないと論じている。だが、それによっていつか巨大な惨事が起きるという話はしていない。
     </p>
   </>
 )
