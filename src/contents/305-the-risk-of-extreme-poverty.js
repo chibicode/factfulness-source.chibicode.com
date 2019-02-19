5846f2bc98876c41ce43dea0a600175d87a7651f
Shu Uesugi <shu@chibicode.com>
2019-02-18 18:44:05 -0800

    Update 305-the-risk-of-extreme-poverty.js

--- src/contents/305-the-risk-of-extreme-poverty.js
+++ src/contents/305-the-risk-of-extreme-poverty.js
@@ -5,71 +5,61 @@ export default () => (
   <>
     <p>
       <H.FB pageNumber={355}>
-        極度の貧困のリスクについては、次の情報源を参考にした:世界銀行(WorldBank[26])、海外開発研究所(ODI)、オスロ国際平和研究所(PRIO)、ポール・コリアー著『最底辺の10億人─最も貧しい人のために本当になすべきことは何か』(2008年6月、中谷和夫訳、日経BP社)、そしてBBCのドキュメンタリー番組“
-        <H.A href="https://www.gapminder.org/videos/dont-panic-end-poverty/">
-          Don’t Panic—End Poverty
-        </H.A>
-        ” (Gapminder[11])
+        極度の貧困のリスクについては、次の情報源を参考にした:
       </H.FB>
-    </p>
-    <p>
-      While extreme poverty has fallen, the number of extremely poor people
-      living in conflict has been stable or even increased, based on preliminary
-      data from PRIO. If current wars continue, the vast majority of extremely
-      poor children will soon live behind military lines. This poses a cultural
-      challenge to the international aid community. The International Dialogue
-      on Peacebuilding and Statebuilding had its fifth global meeting in
-      Stockholm, 2016. The meeting, following the Stockholm Declaration against
-      war and conflict, discussed how the aid community is now
-      <H.A href="https://www.pbsbdialogue.org/en">
-        preparing for the risk of extreme poverty in conflict prone countries
-      </H.A>
-      .
-    </p>
-    <p>
-      The other risks are not ongoing. Extreme poverty is. 78% are small scale
-      farmers in extremely poor areas. In extreme poverty, every second person
-      is a child, see{' '}
-      <H.A href="http://econ.worldbank.org/">World Bank[26]</H.A>, they are from
-      ethnic minorities; see ODI.
-      <H.FB pageNumber={355}>
-        オスロ国際平和研究所の暫定的なデータによると、極度の貧困は減っているが、紛争地帯に暮らす極度に貧しい人の数は変わらないか、増えている。現在の紛争が長引けば、極度に貧困な子供たちの大多数はこれからも紛争地帯から抜け出せない。国際援助団体にとっては、長引く紛争が大きな障害になる。
-      </H.FB>
-      This poses a cultural challenge to the international aid community, as
-      Collier describes in The Bottom Million.
     </p>
     <ul>
-      <li>Poverty leads to civil war, and civil war leads to poverty.</li>
       <li>
-        Conflicts in Afghanistan and central Africa mean that all other
-        sustainability projects in those places are on hold.
-      </li>
-      <li>Terrorists hide in the few remaining areas of extreme poverty.</li>
-      <li>
-        When rhinos are stuck in the middle of a civil war, it's much more
-        difficult to save them.
+        <H.FB pageNumber={355}>
+          世界銀行(
+          <H.A href="https://gapm.io/xwb1726">WorldBank[26]</H.A>)
+        </H.FB>
       </li>
       <li>
-        There's no innovation needed to end poverty. The quicker we act, the
-        smaller the problem (as people in extreme poverty have large families
-        and their numbers keep increasing. )
+        <H.FB pageNumber={355}>
+          <H.A href="https://gapm.io/xodi">海外開発研究所(ODI)</H.A>
+        </H.FB>
       </li>
       <li>
-        The hardest to help will be those stuck behind violent and chaotic armed
-        gangs in weakly governed states.
+        <H.FB pageNumber={355}>
+          <H.A href="">
+            ポール・コリアー著『最底辺の10億人─最も貧しい人のために本当になすべきことは何か』(2008年6月、中谷和夫訳、日経BP社)
+          </H.A>
+        </H.FB>
       </li>
       <li>
-        To escape poverty, they will need a stabilizing military presence of
-        some kind. They will need police officers with guns and government
-        authority to defend innocent citizens against violence and to allow
-        teachers to educate the next generation in peace.
+        <H.FB pageNumber={355}>
+          <H.A href="">
+            BBCのドキュメンタリー番組“
+            <H.A href="https://www.gapminder.org/videos/dont-panic-end-poverty/">
+              Don’t Panic—End Poverty
+            </H.A>
+            ” (Gapminder[11])
+          </H.A>
+        </H.FB>
       </li>
     </ul>
     <p>
-      For a longer list of major risks, see Global Catastrophes and Trends: The
-      Next Fifty Years by Smil (2008). For those who find numbers calming, this
-      is where you will find the big picture of the proportional risks and
-      uncertainties of all kinds of possible fatal discontinuities.
+      <H.FB pageNumber={355}>
+        <H.A href="https://gapm.io/xpriod">
+          オスロ国際平和研究所の暫定的なデータ
+        </H.A>
+        によると、極度の貧困は減っているが、紛争地帯に暮らす極度に貧しい人の数は変わらないか、増えている。現在の紛争が長引けば、極度に貧困な子供たちの大多数はこれからも紛争地帯から抜け出せない。
+      </H.FB>
+    </p>
+    <p>
+      これは国際援助団体にとっては難題だ。ストックホルムで2016年に行われた「第1回平和構築と国家建設に関する国際対話」(
+      <H.A href="https://www.pbsbdialogue.org/en/">
+        International Dialogue on Peacebuilding and Statebuilding
+      </H.A>
+      )では、紛争地域における極度の貧困のリスクと、それを支援する難しさについて言及された。
+    </p>
+    <p>
+      本書で紹介した他の4つのリスクはまだ先の話だが、極度の貧困は今まさに起きているリスクだ。現在、極度の貧困に暮らす人のうち、78%は極度に貧しい地域の小規模農家だ。また、
+      <H.A href="http://econ.worldbank.org/">
+        極度の貧困層のふたりにひとりは子供だ
+      </H.A>
+      。
     </p>
 
     <H.InlineCitation>
