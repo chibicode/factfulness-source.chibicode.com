e7af182bf269cf33e151ec4742e2f3d926a8b92a
Shu Uesugi <shu@chibicode.com>
2019-02-18 22:09:59 -0800

    258

--- src/contents/258-democracy-is-not-the-single-solution.js
+++ src/contents/258-democracy-is-not-the-single-solution.js
@@ -5,34 +5,25 @@ export default () => (
   <>
     <p>
       <H.FB pageNumber={359}>
-        ポール・コリアーの著作は事実に基づいており、同時に恐しいものでもある。レベル1の国では、民主主義によって、国は安全になるどころか不安定になることは、『民主主義がアフリカ経済を殺す─最底辺の10億人の国で起きている事実』(2010年、甘粕智子訳、日経BP社)に詳しく描かれている。
+        レベル1の国では、民主主義によって、国は安全になるどころか不安定になることは、ポール・コリアーの著作『民主主義がアフリカ経済を殺す─最底辺の10億人の国で起きている事実』(2010年、甘粕智子訳、日経BP社)に詳しく描かれている。これは事実に基づいており、同時に恐しいものでもある。
       </H.FB>
-      Unfortunately, this seems to hold true for countries on Level 1, where
-      democracy is promoted by the international community, but leads to
-      destabilization rather than safety. The promotion of so-called
-      "democracy" is well-intended. But in practice, it often involves
-      no more than an "election", and the winner is usually the
-      strongest man with control of the weapons in the violent fight preceding
-      the election. Reality is definitely not as simple as that, but we're
-      describing the disturbing tendency. And to those who lose their loved
-      ones, suffer starvation, violence, miss school, and remain stuck in
-      poverty during endless civil conflicts, it is difficult to convince them
-      about the benefits of democracy, if that's what triggered the conflict.
     </p>
     <p>
-      Collier quotes a threshold of $2700 in GDP per capita per year: over this
-      threshold, democracy lead to more stability, but below it, war was a more
-      likely outcome. That threshold is exactly where the line goes between
-      Level 1 and 2 in Gapminder's income levels.
+      国際団体は、レベル1の国が民主化することを望んでいる。これ自体に悪気はない。しかし民主化をしようとしても、現実的には「選挙もどき」が開かれるだけで、勝者は軍を掌握している者と相場が決まっている。もちろん他のシナリオも起こりうるが、多くの場合は物騒な結果になる。愛する人を亡くした人、飢餓で苦しむ人、学校に行けない人、紛争地帯から抜け出せない人にとって、民主主義の崇高さを説いても説得力はない。そもそも民主主義が紛争を起こしたのであればなおさらだ。
+    </p>
+    <p>
+      コリアーによると、ひとりあたりGDPが2700ドルを超えると、民主主義は国に安定をもたらす。しかしそれ以下だと紛争に繋がりやすい。これはちょうどレベル1とレベル2の中間だ。
+    </p>
+    <p>
       <H.FB pageNumber={359}>
-        民主主義の問題点についてはさらに、Fareed Zakaria著のThe Future of
-        Freedom: Illiberal Democracy at Home and Abroadでも指摘している
+        民主主義の問題点についてはさらに、Fareed Zakariaが著作“The Future of
+        Freedom: Illiberal Democracy at Home and Abroad”でも指摘している。
       </H.FB>
-      In it, Zakarias talks about legislation that has become a question for the
-      angry crowds, who wants tougher punishment for criminals without
-      considering the predicted costs. Read it!
+      Zakariaは、犯罪者への罰則を厳しくする法律を、人々がデメリットを考えずに支援したことについて触れている。
+    </p>
+    <p>
       <H.FB pageNumber={359}>
-        「民主主義がまるで完璧か、万能であるかのように偽ることはできない。実際、民主主義は最悪の政治形態だ。ただし、これまでに試されたほかの政治形態で民主主義よりましなものがなかっただけだ」。
+        わたしたちはウィンストン・チャーチルの次の言葉を肝に銘じておくべきだろう。「民主主義がまるで完璧か、万能であるかのように偽ることはできない。実際、民主主義は最悪の政治形態だ。ただし、これまでに試されたほかの政治形態で民主主義よりましなものがなかっただけだ」。
       </H.FB>
     </p>
   </>
