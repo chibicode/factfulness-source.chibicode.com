9717f2437e8bee09b3800f2b5a6e0e6f8818959c
Shu Uesugi <shu@chibicode.com>
2019-02-19 18:34:41 -0800

    Update 226-high-income-means-low-fertility.js

--- src/contents/226-high-income-means-low-fertility.js
+++ src/contents/226-high-income-means-low-fertility.js
@@ -3,24 +3,17 @@ import * as H from '../components/h'
 
 export default () => (
   <p>
-    Fertility rates for global religions are based on estimates from PEW[3],
-    calculating fertility across all income levels. The average fertility rate
-    for global religions are based on
+    各宗教ごとの平均出生率は
     <H.A href="http://www.pewforum.org/2015/04/02/religious-projections-2010-2050/">
-      population growth projections, 2010--2050
+      PEW[3]
     </H.A>
-    from PEW[3], calculating fertility rate of each religion, across all income
-    levels. According to their estimates, the highest fertility rate is among
-    Muslims, with a rate at 3.1 children per woman; second highest is Christian
-    fertility, with a rate at 2.7; Hindu fertility is at 2.4, which is closer to
-    the global average; Jewish fertility at 2.3.
+    によるもので、全ての所得レベルにおける出生率を計算している。出生率が最も高いのはイスラム教徒(3.1)。次いでキリスト教徒(2.7)、ヒンドゥー教徒(2.4)、ユダヤ教徒(2.3)の順に高い。
     <H.A href="http://www.pewforum.org/2015/04/02/religious-projections-2010-2050/pf_15-04-02_projectionsoverview_totalfertility_640px/">
-      Total Fertility Rate by Religion, 2010-2015
+      世界平均に近いのはヒンドゥー教徒だ
     </H.A>
-    or
+    。PEWの報告書は
     <H.A href="http://assets.pewresearch.org/wp-content/uploads/sites/11/2015/03/PF_15.04.02_ProjectionsFullReport.pdf">
-      download the complete report here
+      こちらからダウンロードできる。
     </H.A>
-    .
   </p>
 )
