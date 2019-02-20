8c00b0f8595796fd3f9d6c8b981b75d2291b96cb
Shu Uesugi <shu@chibicode.com>
2019-02-20 00:29:05 -0800

    Update 221-80-years-ago-in-sweden-and-us.js

--- src/contents/221-80-years-ago-in-sweden-and-us.js
+++ src/contents/221-80-years-ago-in-sweden-and-us.js
@@ -2,19 +2,20 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    In the US in the 1930s, the
-    <H.A href="https://en.wikipedia.org/wiki/Farm_Security_Administration">
-      Farm Security Administration
-    </H.A>
-    (FSA) sent photographers across the nation to understand what it looked
-    like, resulting in famous black and white images from the rural poor. In
-    Sweden in 1938, Ludvig "Lubbe" Nordström gave eye witness reports
-    to radio listeners on the national radio report series called Dirt Sweden.
-    It is described in the article "Culture, Health, and Religion at the
-    Millennium: Sweden Unparadised" by Demker et al. (2014). These human
-    faces to mobilized support for massive development investments across these
-    countries, leading to the Swedish welfare system being constructed and the
-    New Deal in the US.
-  </p>
+  <>
+    <p>
+      1930年代のころ、アメリカの農業安定局 (
+      <H.A href="https://ja.wikipedia.org/wiki/FSA%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88">
+        FSA
+      </H.A>
+      )は国中に写真家を派遣し、大恐慌後の農村の惨状を白黒写真で伝えた。一方1938年のスウェーデンでは、Ludvig
+      "Lubbe" Nordströmが国営ラジオのDirt
+      Swedenという企画にて、各地の悲惨さを生々しく伝えた。このことはDemker et
+      al著"Culture, Health, and Religion at the Millennium: Sweden Unparadised"
+      (2014)という記事に詳しく書かれている。
+    </p>
+    <p>
+      こうした取り組みが政治を動かし、国中で開発プロジェクトがはじまった。スウェーデンでは福祉の仕組みがつくられ、アメリカではニューディール政策が施行された。
+    </p>
+  </>
 )
