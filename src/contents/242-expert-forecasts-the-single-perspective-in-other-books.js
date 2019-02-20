3aafd5d706c132c4c5aced55aa8d46320e956d57
Shu Uesugi <shu@chibicode.com>
2019-02-19 16:18:18 -0800

    Update 242-expert-forecasts-the-single-perspective-in-other-books.js

--- src/contents/242-expert-forecasts-the-single-perspective-in-other-books.js
+++ src/contents/242-expert-forecasts-the-single-perspective-in-other-books.js
@@ -2,19 +2,15 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    People with extraordinary expertise in one field score just as badly on our
-    fact questions as everyone else. This wouldn't surprise Philip E. Tetlock
-    and Dan Gardner, the authors of Superforecasting (2015). In this book they
-    describe a systematic way to test people's ability to predict the future,
-    and they find that one thing that can really impair good judgment is narrow
-    expertise. Even forecasts by so-called experts in the media are often overly
-    dramatic. When checking against actual outcomes, Tetlock and Gardner show
-    that the experts not only score worse than the public but also worse than
-    random. The personality traits that often come with good judgment are
-    humility, curiosity, and a willingness to learn from mistakes. The Good
-    Judgement Project, a forecasting services firm co-created by Tetlock, runs a
-    public tournament called Good Judgement Open. You can practice your
-    forecasting here: <H.A href="https://www.gjopen.com/">www.gjopen.com</H.A>.
-  </p>
+  <>
+    <p>
+      何かの分野に突出した専門家でも、ギャップマインダーのクイズには他の人並みに間違える。
+      フィリップ・E・テトロック、ダン・ガードナーは、著作『超予測力』(2016年、土方奈美訳、早川書房)でまさにこの現象について書いている。彼らは人々に未来を予測してもらったところ、狭い領域での専門知識があると予測力が落ちるという結果が出た。しかも、専門家による予測はふつうの人どころかチンパンジーより悪い結果だった。
+    </p>
+    <p>
+      予測力を育むには、謙虚さ、好奇心、そして間違いから学ぼうとする姿勢が大事だ。テトロックが立ち上げた予測コンサルティング会社のGood
+      Judgementは、Good Judgement Openという予測のトーナメントを行なっており、
+      <H.A href="https://www.gjopen.com/">誰でもこちらから参加できる</H.A>。
+    </p>
+  </>
 )
