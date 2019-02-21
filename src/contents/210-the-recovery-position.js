d7f51e216d773b1396c4c7624c80f9e8752d0530
Shu Uesugi <shu@chibicode.com>
2019-02-20 22:51:51 -0800

    Update 210-the-recovery-position.js

--- src/contents/210-the-recovery-position.js
+++ src/contents/210-the-recovery-position.js
@@ -2,17 +2,20 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    Before the 1950s, war surgery guidelines didn't have a specific body
-    position recommended for the care of trauma patients. When the so-called
-    NATO coma position had been successfully used by the US in the Korean War,
-    however, it began to be promoted for saving unconscious soldiers; see
-    Högberg and Bergström (1997). The recovery position was not standardized
-    until decades later, in the early 1990s, when it emerged in general first
-    aid handbooks. See
-    <H.A href="https://en.wikipedia.org/wiki/Recovery_position">
-      Wikipedia[10]
-    </H.A>
-    for more on the history of the recovery position.
-  </p>
+  <>
+    <p>
+      1950年代以前のころ、意識を失った兵士をどのように寝かせるべきかを示すガイドラインは無かった。だが、1950年に起きた朝鮮戦争では、“The
+      NATO coma
+      position”と呼ばれる回復体位が使われるようになった。この結果、意識を失った兵士の生存率が上がった。このことは
+      <H.A href="https://gapm.io/xuhsid">Högberg and Bergström (1997)</H.A>
+      に詳しい。
+    </p>
+    <p>
+      回復体位がどの応急処置マニュアルにも載るようになったのは数十年後の1990年代初頭だった。詳しくは
+      <H.A href="https://en.wikipedia.org/wiki/Recovery_position">
+        Wikipedia[10]
+      </H.A>
+      を参考のこと。
+    </p>
+  </>
 )
