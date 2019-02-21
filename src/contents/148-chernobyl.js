f7824ad7b056d275b3040cc45ab4602138643542
Shu Uesugi <shu@chibicode.com>
2019-02-21 02:59:18 -0800

    Update 148-chernobyl.js

--- src/contents/148-chernobyl.js
+++ src/contents/148-chernobyl.js
@@ -4,23 +4,11 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      <H.FB pageNumber={364}>
-        チェルノブイリの調査についてはWHO[5]を参照のこと
-      </H.FB>
-      In 2016, the World Health Organization published "1986-2016:
-      Chernobyl at 30 -- an update" and they report that they couldn't
-      confirm any large increase in cancers, like thyroid cancer, that many had
-      expected in the population in general. Instead, they conclude: "The
-      psycho-social impact of disasters and emergencies has been well
-      documented. It has been reported to be the Chernobyl accident main public
-      health impact that affected the largest number of people. A similar effect
-      is now reported in the aftermath of the Fukushima disaster, "
-      reporting that "anxiety levels that were twice as high than
-      non-exposed population, and were more likely to report multiple
-      unexplained physical symptoms and subjective poor health." Read more
-      about WHO's summarizing reports on the healths effects of the Chernobyl
-      accident, here:
-      <H.A href="http://gapm.io/xwhoc30">http://gapm.io/xwhoc30</H.A>.
+      2016年に、世界保健機関は
+      <H.A href="http://gapm.io/xwhoc30">
+        "1986-2016: Chernobyl at 30 -- an update"
+      </H.A>
+      という報告書を公開した。報告書によると、人々の恐れとは裏腹に、甲状腺癌の発生率の増加は見られなかった。一方、チェルノブイリの最も大きな被害は、人々の心理的・社会的な被害だったという。不安症を発症する確率が事故と無関係の人に比べ倍になったり、原因不明の身体の不調が起きる人が多くなったという。
     </p>
     <p>
       <H.FB pageNumber={364}>
