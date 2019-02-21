75b18166fd494e5f6df8bdf366a9928ed2a2b49c
Shu Uesugi <shu@chibicode.com>
2019-02-21 00:57:05 -0800

    Update 173-bears-and-axes.js

--- src/contents/173-bears-and-axes.js
+++ src/contents/173-bears-and-axes.js
@@ -13,28 +13,16 @@ export default () => (
       </H.FB>
     </p>
     <p>
-      In a dissertation from Karolinska Institutet, Shilan Caman (2017) studied
-      domestic violence in Sweden, summarizing on page 82 that:
-    </p>
-    <p>
-      "Enligt Brå hade cirka 17 kvinnor dödats varje år av en man i en nära
-      relation under perioden 1990 -- 2005. Detta var en minskning jämfört med
-      perioden innan [6]. I en rapport från 2015 redovisar Brå en fortsatt
-      minskning. Under perioden 2008 -- 2013 var antalet fall 13 kvinnor per år.
-      Sedan början på 2000-talet har minskningen varit 20 procent."
-      [According to Brå, about 17 women was killed every year by a man in a
-      close relationship during the period of 1990--2005. This was a decrease in
-      comparison to the period before. In a report from 2015, Brå presents a
-      continuing decrease. For 2008--2013, the number was 13 women per year. The
-      decrease has been 20 percent since the beginning of the 2000s.]
-    </p>
-    <p>
-      Caman's study called Intimate Partner Homicide Rates and Characteristics
-      is in English and can be
+      Shilan
+      Camanは、スウェーデンのDVについてカロリンスカ医科大学の学術論文で以下のように記した。
+      (論文のリンクはこちら:{' '}
       <H.A href="https://openarchive.ki.se/xmlui/bitstream/handle/10616/45867/Thesis_Shilan_Caman.pdf?sequence=1&amp;isAllowed=y#__utma=1.120245944.1513028453.1513028453.1513028453.1&amp;__utmb=1.2.10.1513028453&amp;__utmc=1&amp;__utmx=-&amp;__utmz=1.1513028453.1.1.utmcsr=google%7Cutmccn=(organic)%7Cutmcmd=organic%7Cutmctr=(not%20provided)&amp;__utmv=-&amp;__utmk=259404170">
-        downloaded as PDF
+        Intimate Partner Homicide Rates and Characteristics
       </H.A>
-      .
+      )。
+    </p>
+    <p>
+      「Bråによると、1990年から2005年の間に、毎年17人の女性が近しい関係の男性に殺されている。この数字は、その前の期間より減っていた。またBråによると、2008年から2013年の間を見ると、殺された女性の数は毎年13人に減っていた。2000年代初頭から約2割減っていることになる」
     </p>
   </>
 )
