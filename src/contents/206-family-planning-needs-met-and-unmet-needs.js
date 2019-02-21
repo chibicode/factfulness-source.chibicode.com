a86103b01ef16f0802c163145e9448f28bcc7b5a
Shu Uesugi <shu@chibicode.com>
2019-02-20 23:36:58 -0800

    Update 206-family-planning-needs-met-and-unmet-needs.js

--- src/contents/206-family-planning-needs-met-and-unmet-needs.js
+++ src/contents/206-family-planning-needs-met-and-unmet-needs.js
@@ -9,29 +9,25 @@ export default () => (
         <H.A href="http://www.unfpa.org/sexual-reproductive-health">UNFPA</H.A>
         と<H.A href="https://gapm.io/xcontr">UN-Pop[9]</H.A>)。
       </H.FB>
-      国連人口基金 is the part of the United Nations that deals with sexual and
-      reproductive health. The data show the unmet need for family planning and
-      are based on estimates from the UN data set, World Contraceptive Use 2017.
-      The data set provides estimates for all women of reproductive age, 15--49
-      years, who are married or in a union. See, for example,
-      <H.A href="https://data.worldbank.org/indicator/SP.DYN.CONM.ZS?locations=XN-XT-XD-XM">
-        contraceptive prevalence rate
-      </H.A>
-      for the select countries, along with more details. You can explore the
-      data in
-      <H.A href="https://www.unfpa.org/sexual-reproductive-health">
-        the interactive chart
-      </H.A>
-      further down the page.
+      ちなみに国連人口基金は性と生殖に関する健康を推進する国連の団体だ。
     </p>
     <p>
-      While we report the percentages of women who say their needs for
-      contraceptives are met, UN publishes the reverse numbers, as rates of
-      unmet need for family planning. It is defined as the share of women who
-      don't want to become pregnant, or want to postpone pregnancy, but are not
-      using any method of contraception. The data set, World Contraceptive Use
-      2017, is available here:
-      <H.A href="http://www.gapm.io/xcontr">www.gapm.io/xcontr</H.A>.
+      ここでは国連の
+      <H.A href="http://www.un.org/en/development/desa/population/publications/dataset/contraception/wcu2017.shtml">
+        World Contraceptive Use 2017
+      </H.A>
+      という、家族計画に必要なものが手に入るかについてのデータセットを用いている。対象は子供を産む年齢(15〜49歳)で配偶者がいる女性だ。ちなみに本書では避妊具が「手に入る」女性の割合を書いているが、国連は避妊具が「手に入らない」女性の割合を公開している。これは妊娠したくない、または妊娠を遅らせたいと考える女性のうち、避妊具を使っていない女性が占める割合だ。
+    </p>
+    <p>
+      また、国ごとの
+      <H.A href="https://data.worldbank.org/indicator/SP.DYN.CONM.ZS?locations=XN-XT-XD-XM">
+        避妊具の普及率はこちらから見れる
+      </H.A>
+      。こちらの
+      <H.A href="https://www.unfpa.org/sexual-reproductive-health">
+        動くチャート
+      </H.A>
+      も参考のこと。
     </p>
   </>
 )
