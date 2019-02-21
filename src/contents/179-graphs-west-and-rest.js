2a8429996201a2ba4747c91cdd5bdff8e4caba97
Shu Uesugi <shu@chibicode.com>
2019-02-21 00:19:20 -0800

    Update 179-graphs-west-and-rest.js

--- src/contents/179-graphs-west-and-rest.js
+++ src/contents/179-graphs-west-and-rest.js
@@ -2,19 +2,16 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    On these graphs, where the world population is divided into west and rest,
-    we refer to what we think the student in the European classroom had in mind
-    --- the west consisting predominantly of Western Europe, United States,
-    Canada, and Australia. Gapminder's rough estimates of the number of people
-    on different incomes for 2017 are based on
-    <H.A href="http://iresearch.worldbank.org/PovcalNet">
-      World Bank official data from PovCalNet
-    </H.A>
-    , and extended into the future with IMF forecasts. To see how the majority
-    of people are catching up with the west, see the color-chart at the end of
-    the book, showing the number of people on each income level separated by the
-    four regions, or use this interactive version to animate the income
-    mountains: <H.A href="http://www.gapm.io/incm">www.gapm.io/incm</H.A>.
-  </p>
+  <>
+    <p>
+      このページのグラフの「西洋諸国」は、典型的なヨーロッパの学生がイメージするであろう「西洋諸国」を表している。それらは西ヨーロッパ、アメリカ、カナダ、オーストラリアだ。2017年の所得ごとの人口は
+      <H.A href="http://iresearch.worldbank.org/PovcalNet">PovCalNet</H.A>
+      を基にしており、未来のデータはIMFの予測を基にしている。
+    </p>
+    <p>
+      巻末の見開きにも、未来の所得ごとの人口が載っている。こちらの
+      <H.A href="http://www.gapm.io/incm">アニメーションチャート</H.A>
+      もぜひ参考にしてほしい。
+    </p>
+  </>
 )
