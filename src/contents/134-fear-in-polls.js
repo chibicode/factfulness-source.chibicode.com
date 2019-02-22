328a0693dfc2c63455516151e6f68dc6f557b42b
Shu Uesugi <shu@chibicode.com>
2019-02-21 19:27:53 -0800

    Update 134-fear-in-polls.js

--- src/contents/134-fear-in-polls.js
+++ src/contents/134-fear-in-polls.js
@@ -2,18 +2,17 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    In the passage about the most common fears, we refer to those self-reported
-    among adults. The polls, conducted in the United States and the United
-    Kingdom respectively, reported similar results. In the Gallup[2] survey, top
-    US fears were snakes; public speaking; and heights, followed by entrapment;
-    spiders and insects; needles; ice; flying; and dogs. In the poll from
-    YouGov[2], the leading fears in the United Kingdom were heights; snakes; and
-    public speaking; followed by spiders; entrapment; mice; needles; and
-    airplanes. Read more about the fear instinct at
-    <H.A href="https://www.gapminder.org/factfulness.org/fear">
-      www.gapminder.org/factfulness.org/fear
-    </H.A>
-    .
-  </p>
+  <>
+    <p>
+      文中で紹介している、恐れられているもののリストは、アメリカとイギリスそれぞれで大人向けに行われた調査を基にしている。
+    </p>
+    <p>
+      <H.A href="http://news.gallup.com/poll/1891/snakes-top-list-americans-fears.aspx">
+        Gallup[2]
+      </H.A>
+      がアメリカで行なった調査によると、上位にランクインしたのは「ヘビ」「人前で話すこと」「高所」「閉所」「クモなどの虫」「針」「飛行機」「犬」だった。一方、
+      <H.A href="https://gapm.io/xyougov15">YouGov[2]</H.A>
+      がイギリスで行なった調査によると、上位にランクインしたのは「高所」「ヘビ」「人前で話すこと」「クモ」「閉所」「ネズミ」「針」「飛行機」だった。
+    </p>
+  </>
 )
