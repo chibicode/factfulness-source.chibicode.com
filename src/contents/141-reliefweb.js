4fd9d5cffac5ba3a0876cfbba9859fbd57ee8302
Shu Uesugi <shu@chibicode.com>
2019-02-21 18:37:57 -0800

    Update 141-reliefweb.js

--- src/contents/141-reliefweb.js
+++ src/contents/141-reliefweb.js
@@ -2,21 +2,19 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    A specialized digital service of the UN Office for the Coordination of
-    Humanitarian Affairs (OCHA), the ReliefWeb is a global humanitarian
-    information platform. On their
-    <H.A href="https://reliefweb.int/faq">FAQ page</H.A> they explain that their
-    main function is to provide humanitarian workers with key information on
-    global crises and disasters. The ReliefWeb has a yearly budget of $3.8
-    million, which is funded by the OCHA and include contributions from Sweden,
-    Japan, the United States, the United Kingdom, Denmark and others. In
-    detailed reports, the ReliefWeb
-    <H.A href="http://www.gapm.io/xreliefwb">
-      describes how the money was used
-    </H.A>
-    , stating that "the Red Crescent is also working with the United
-    Nations' World Food Programme to distribute 112.5 tonnes of biscuits to
-    30,000 families in Cox's Bazar."
-  </p>
+  <>
+    <p>
+      リリーフウェブは、国連人道問題調整事務所が提供する人道支援の情報サイトだ。
+      <H.A href="https://reliefweb.int/faq">FAQ</H.A>
+      ページによると、リリーフウェブの主な仕事は、人道支援者に向けて世界の紛争や災害の状況を発信することだ。
+    </p>
+    <p>
+      リリーフウェブの年間予算は約380万ドルで、国連人道問題調整事務所と、スウェーデン・日本・アメリカ・イギリス・デンマークなどから資金提供を受けている。
+    </p>
+    <p>
+      リリーフウェブには、本書で出てきたバングラデシュの洪水についての記事もある。
+      <H.A href="http://www.gapm.io/xreliefwb">こちらの記事によると</H.A>
+      、赤十字社と世界食糧計画が112.5トンのビスケットを被害を受けた3万世帯に配った、とある。
+    </p>
+  </>
 )
