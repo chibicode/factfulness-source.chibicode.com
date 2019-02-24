5a3b036e9397a60c5397e80a70a019454f708994
Shu Uesugi <shu@chibicode.com>
2019-02-24 02:41:03 -0800

    Old balance

--- src/contents/112-the-old-balance.js
+++ src/contents/112-the-old-balance.js
@@ -5,48 +5,31 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      Gapminder presents the old balance as a family with 6 children, out of
-      which 4 die. This iconic family represents a theoretical average, measured
-      over the millennia with wide ranges in mortality and fertility. In the
-      long time-frame, however, the average family probably had no more than two
-      surviving children, since population growth on average stayed stable until
-      1800.
+      本書では、自然との調和が取れていた過去を、「女性ひとりあたりの子供の数は6人で、うち4人が大人になるまでに亡くなった」と書いている。これは当時における理論上の平均的な家族を表している。もちろん、何千年もの間、死亡率や出生率は大きく上下していた。しかし、1800年までは人口がほとんど増えなかったことから、平均的に見れば一家庭あたりふたりしか大人になれなかったはずだ。
     </p>
     <p>
-      <H.FB pageNumber={366}>1800年より前の出生率を知る者はいない。</H.FB> In
-      theory, the maximum number of children per woman would be between 10 and
-      15 children if all fertile women in a healthy population had sex often and
-      continuously with no attempts to limit fertility. In reality, it is almost
-      always lower than that, and six children per woman is a likely average.
-      But fertility varies between groups and individual women and, as Gurven
-      and Kaplan writes, it "ranges widely from below 4 to as high as 8
-      children per woman." (2007, p. 347)
+      <H.FB pageNumber={366}>1800年より前の出生率を知る者はいない。</H.FB>
+      全ての女性が健康で、性行為を頻繁に行い、家族計画を一切行わなければ、ひとりあたりの女性の数は10人から15人になる。理論上はそうだが、現実的にこんなに多くなることはない。過去の平均はおそらく6人あたりだろう。もちろん、全員がそうだったわけではなく、
+      <H.A href="https://gapm.io/xhun">Gurven and Kaplan (2007, p347)</H.A>
+      が言うように「4人以下だった社会もあり、8人以上だった社会もあった」だろう。
     </p>
     <p>
-      Age specific mortality rates of the first millennials of human history are
-      equally uncertain, but we can assume that over these huge time-horizons,
-      mortality rate was high enough to keep population sizes down; see
-      Livi-Bacci (1989). Mortality was unpredictable. Sudden catastrophes,
-      caused by famines, wars or pandemics, killed many and became much more
-      common with agriculture and crowded settlements, or through contact with
-      other populations that brought germs with them. For regular years, between
-      these bad periods, estimates vary from 32% to 45% of deaths in children
-      under-5 years, which is a similar rate to that of the hunter-gatherer
-      societies studied in modern times. Gurven and Kaplan (2007) writes that:
+      過去の年齢別の死亡率を知る者もいない。ただ、人口があまり増えなかったことから、人類史がはじまったころの数千年間、死亡率はとても高かったのだろう。詳しくはリヴィ-バッチ著『人口の世界史』(2014年、速水融・斎藤修訳、東洋経済新報社)を参照のこと。
     </p>
     <p>
-      "The regular mortality rates differ among populations and among
-      periods, especially in risks of violent death. However, those differences
-      are small in a comparative cross-species perspective, and the similarity
-      in mortality profiles of traditional peoples living in varying
-      environments is impressive."
+      命を落とす理由も様々だった。飢餓・戦争・病気は多くの人の命を奪った。農耕が盛んになったり、人口が密集したり、他の民族と交わるようになると、病原菌による感染症の数も増えた。極端に多くの人が亡くなった時期以外の乳幼児死亡率の推定は32%から45%とさまざまだ。これは、最近の狩猟採集社会の乳幼児死亡率とあまり変わらない。
+      <H.A href="https://gapm.io/xhun">Gurven and Kaplan (2007)</H.A>
+      は、大昔の死亡率に対して以下のように記している:
     </p>
     <p>
-      Before 1800, when agriculture made people crowd in settlements and
-      diseases from domesticated animals killed many, child mortality in regular
-      years was probably above 35%. The main sources behind our assumptions
-      about fertility and mortality in pre-1800 families are Livi-Bacci (1989),
-      Paine and Boldsen (2002), and Gurven and Kaplan (2007).
+      もちろん、時期によって死亡率や殺人率は違った。ただ、人類全体で見ると死亡率の差はあまり変わらない。さまざまな環境に暮らしていた人々の死亡率がほとんど変わらなかったというのは特筆すべきだ。
+    </p>
+    <p>
+      また過去の人口については、
+      <H.A href="https://www.cambridge.org/core/books/paleodemography/linking-ageatdeath-distributions-and-ancient-population-dynamics-a-case-study/BFE97596BED2E91300F4DDB4DDEBFA90">
+        Paine and Boldsen (2002)
+      </H.A>
+      も参考にした。
     </p>
   </>
 )
