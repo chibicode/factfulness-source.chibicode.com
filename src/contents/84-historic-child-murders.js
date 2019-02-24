e023578d49825e7dac1cdeb9fcb9f8a3ebb0c885
Shu Uesugi <shu@chibicode.com>
2019-02-24 12:57:06 -0800

    Update 84-historic-child-murders.js

--- src/contents/84-historic-child-murders.js
+++ src/contents/84-historic-child-murders.js
@@ -4,28 +4,30 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      All groups of hunter-gatherers and hunter-horticulturalists have not been
-      equally brutal. At some archaeological sites, the violent death rates are
-      lower than in most violent cities today, at 1%. But that is uncommon.
-      Instead, violence among hunter-gatherers were probably at least 10 times
-      more common. For 39 hunter-gatherers populations where archaeologists and
-      anthropologists have assessed the causes of death, they have found flint
-      arrowheads in chests and holes in skulls. And on average, the estimated
-      murder rate for these sites was at 16%; see Pinker[3] and
-      OurWorldInData[7]. Skeletons found in excavations of prehistoric massacres
-      show that children were treated just as brutally as adults. Infanticide is
-      found in many human groups with food scarcity across the world; see
-      Pinker[4]. The hunter-gatherer groups studied by Gurven and Kaplan (2007)
-      showed that deaths under age-15 were on average caused by violence.
-    </p>
-    <p>
       <H.FB pageNumber={367}>
-        暴力が日常的に起きる社会では、子供とはいえ命の保証はない。一般的に、狩猟採集社会では暴力による危険が多い(Gurven
-        and Kaplan(2007)、ダイアモンド(2013)、ピンカー(2015)、
+        暴力が日常的に起きる社会では、子供とはいえ命の保証はない。一般的に、狩猟採集社会では暴力による危険が多い(
+        <H.A href="https://gapm.io/xhun">Gurven and Kaplan(2007)</H.A>
+        、ダイアモンド『昨日までの世界』(2013)、ピンカー『暴力の人類史』(2015)、
         <H.A href="https://www.ourworldindata.org/ethnographic-and-archaeological-evidence-on-violent-deaths">
           OurWorldInData[5]
         </H.A>
-        )。かといって、どの狩猟採集社会も似たようなものとは言い切れない。また、極度の貧困に暮らしている人々の中では、文化の違いにかかわらず、「子殺し」が行われることがある。子殺しとは、飢えで食料不足に陥ったとき、親が子を口減らしのために殺すことをいう。昔ながらの村に行き、子殺しをした親から話を聞いた人類学者たちの多くは、「子殺しで子供を失うつらさは、他の原因で子供を失うのと変わらないようだ」と報告している。詳しくはピンカー(2013)を参照のこと。
+        )。かといって、どの狩猟採集社会も似たようなものとは言い切れない。
+      </H.FB>
+    </p>
+    <p>
+      暴力による死亡率が約1%だったとわかる遺跡もある。この死亡率は、現在の最も治安が悪い都市より低い。だが、そんなケースは稀だ。暴力による死亡率は、最低でも平均10%以上だったはずだ。考古学者や人類学者たちが39の狩猟採集社会における主な死因をを調べたところ、石製の矢尻が骨に埋まっていることに気づいた。それをもとに推定された殺人による死亡率は16%だった。詳しくはスティーブン・ピンカー著『暴力の人類史』(2015年、幾島幸子・塩原通緒訳、青土社)、
+      <H.A href="https://ourworldindata.org/ethnographic-and-archaeological-evidence-on-violent-deaths">
+        OurWorldInData[5]
+      </H.A>
+      を参照のこと。
+    </p>
+    <p>
+      また、<H.A href="https://gapm.io/xhun">Gurven and Kaplan(2007)</H.A>
+      は、狩猟採集社会で15歳未満が亡くなる主な要因は暴力だったと示している。
+    </p>
+    <p>
+      <H.FB pageNumber={367}>
+        また、極度の貧困に暮らしている人々の中では、文化の違いにかかわらず、「子殺し」が行われることがある。子殺しとは、飢えで食料不足に陥ったとき、親が子を口減らしのために殺すことをいう。昔ながらの村に行き、子殺しをした親から話を聞いた人類学者たちの多くは、「子殺しで子供を失うつらさは、他の原因で子供を失うのと変わらないようだ」と報告している。詳しくはピンカー(2013)を参照のこと。
       </H.FB>
     </p>
     <H.InlineCitation>
