7f743eb7edf6ebad81839ccff7b0fe9a8eaa71f5
Shu Uesugi <shu@chibicode.com>
2019-02-24 21:57:46 -0800

    Update 28-child-mortality-in-1995-and-2017.js

--- src/contents/28-child-mortality-in-1995-and-2017.js
+++ src/contents/28-child-mortality-in-1995-and-2017.js
@@ -5,26 +5,24 @@ export default () => (
   <>
     <p>
       <H.FB pageNumber={370}>
-        1995年の講義で使った乳幼児死亡率はユニセフによるもの(UNICEF[1])。本書では当初よりも新しい2017年のデータを使っている(
-        <H.A href="http://www.childmortality.org/">UN-IGME</H.A>)
+        1995年の講義で使った乳幼児死亡率はユニセフによるもの(
+        <H.A href="https://www.unicef.org/about/history/files/sowc_1995.pdf">
+          UNICEF[1]
+        </H.A>
+        )。本書では当初よりも新しい2017年のデータを使っている。
       </H.FB>
-      ---a data collaboration between UNICEF, WHO, UN Population Division and
-      the World Bank. The numbers used in the evening lecture back in 1995 were
-      only slightly different: Saudi Arabia in 1960 was 292 and in 1993, 38;
-      Malaysia in 1960 was 105 and in 1993, 17; Brazil in 1960 was 181 and in
-      1993, 63; Tanzania in 1960 was 249 and in 1993, 16.
+      これは
+      <H.A href="http://www.childmortality.org/">UN-IGME</H.A>
+      によるもので、ユニセフ、世界保健機関、国連、世界銀行の合同データプロジェクトだ。
     </p>
     <p>
-      The data from UNICEF[1] is based on The State of the World's Children
-      1995, published by Oxford U P, available to download as
-      <H.A href="https://www.unicef.org/about/history/files/sowc_1995.pdf">
-        PDF here
-      </H.A>
-      . Data for 2017 is derived from "Child Mortality Estimates" from{' '}
-      <H.A href="http://www.childmortality.org/">www.childmortality.org</H.A>.
-      UN-IGME---United Nations Inter-agency Group for Child Mortality
-      Estimation---is a data collaboration between UNICEF, WHO, UN Population
-      Division and the World Bank.
+      1995年のハンスの講義で使った乳幼児死亡率の推定は、現在の推定とほとんど変わらない。1995年の推定はこちら:
     </p>
+    <ul>
+      <li>サウジアラビア: 1000人中292人 (1960年) → 38人 (1993年)</li>
+      <li>マレーシア: 105人 (1960年) → 17人 (1993年)</li>
+      <li>ブラジル: 181人 (1960年) → 63人 (1993年)</li>
+      <li>タンザニア: 249人 (1960年) → 167人 (1993年)</li>
+    </ul>
   </>
 )
