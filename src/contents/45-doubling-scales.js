2d4bd20119abdc5e11ccd49ec84584fc9734d63f
Shu Uesugi <shu@chibicode.com>
2019-03-02 02:42:07 -0800

    Update 45-doubling-scales.js

--- src/contents/45-doubling-scales.js
+++ src/contents/45-doubling-scales.js
@@ -2,20 +2,20 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    <H.FB pageNumber={369}>
-      本書では、人々の所得レベルや国の平均所得を比べるときは、グラフの軸に倍増する目盛を利用している。
-    </H.FB>
-    1ドルの価値は、所得レベルによって大きく異なるからだ。地震の強さを表すマグニチュード、音量を表すデシベル、酸性やアルカリ性を表すpHなど、
-    <H.FB pageNumber={369}>
-      小さい数字同士の小さな差と、大きい数字同士の大きな差が同じくらい重要なとき、倍増する目盛(対数目盛)は役に立つ。
-    </H.FB>
-    It's not a mistake, it's not cheating. It's representing the world as it
-    really is: in this case, a world where it is not size of the pay rise that
-    matters, but the size of the pay rise in relation to what you had before.
-    Logarithmic scales are based on the theory of diminishing marginal value,
-    first proposed by Daniel Bernoulli in 1738, as Kahneman (2011) describes in
-    his book Thinking, Fast and Slow (p. 274). Doubling scales are explained
-    with more examples in Factfulness on page 98.
-  </p>
+  <>
+    <p>
+      <H.FB pageNumber={369}>
+        本書では、人々の所得レベルや国の平均所得を比べるときは、グラフの軸に倍増する目盛を利用している。
+      </H.FB>
+      1ドルの価値は、所得レベルによって大きく異なるからだ。地震の強さを表すマグニチュード、音量を表すデシベル、酸性やアルカリ性を表すpHなど、
+      <H.FB pageNumber={369}>
+        小さい数字同士の小さな差と、大きい数字同士の大きな差が同じくらい重要なとき、倍増する目盛(対数目盛)は役に立つ。
+      </H.FB>
+    </p>
+    <p>
+      対数目盛を使うのは恣意的ではない。対数目盛を使う方が、現実を正しくグラフで表すことができるからだ。たとえば収入がアップする場合、上がったぶんの絶対値ではなく、以前の収入に比べて何割増えたかのほうが大事だ。これを限界効用逓減の法則と呼び、グラフで表すには対数目盛が適している。ダニエル・カーネマン著『ファスト&スロー』(2014年、村井章子訳、早川書房)によると、1738年にDaniel
+      Bernoulliがこの概念をはじめに提唱した。
+    </p>
+    <p>また、対数目盛に関しては本書でも123ページに詳しく書いてある。</p>
+  </>
 )
