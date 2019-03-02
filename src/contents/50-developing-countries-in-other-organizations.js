7e232cba201d37020ab61607e4650c658eef4797
Shu Uesugi <shu@chibicode.com>
2019-03-02 14:59:10 -0800

    Update 50-developing-countries-in-other-organizations.js

--- src/contents/50-developing-countries-in-other-organizations.js
+++ src/contents/50-developing-countries-in-other-organizations.js
@@ -12,12 +12,18 @@ export default () => (
     </p>
     <ul>
       <li>
-        <strong>難民の84%は途上国に暮らしている (2017年6月20日):</strong>{' '}
+        <strong>
+          <H.A href="https://www.weforum.org/agenda/2017/06/eighty-four-percent-of-refugees-live-in-developing-countries/">
+            難民の84%は途上国に暮らしている (2017年6月20日):
+          </H.A>
+        </strong>{' '}
         難民の数はUNHCRによるもの。「途上国」とは、時代遅れの国連統計部の定義を指す。{' '}
       </li>
       <li>
         <strong>
-          グローバル貿易を途上国に役立てるための5つの方法 (2016年9月):
+          <H.A href="https://www.weforum.org/agenda/2016/09/global-trade-developing-countries-unctad/">
+            グローバル貿易を途上国に役立てるための5つの方法 (2016年9月):
+          </H.A>
         </strong>{' '}
         ここでは、「途上国」は
         <H.A href="https://www.un.org/development/desa/dpad/least-developed-country-category.html">
@@ -26,7 +32,11 @@ export default () => (
         のことを指す。ひとりあたりの所得、資産、経済の脆弱さを考慮に入れている。
       </li>
       <li>
-        <strong>電子化が途上国を救う(2017年7月):</strong>{' '}
+        <strong>
+          <H.A href="https://www.weforum.org/agenda/2017/07/digital-least-developed-countries-inequality/">
+            電子化が途上国を救う(2017年7月):
+          </H.A>
+        </strong>{' '}
         途上国の定義はすぐ上の記事と同じ。
       </li>
     </ul>
