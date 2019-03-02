0031a2e17b3d6b9dfafbb864bd60b65e42b4e3f6
Shu Uesugi <shu@chibicode.com>
2019-03-02 02:24:17 -0800

    Update 42-where-75-of-humanity-lives.js

--- src/contents/42-where-75-of-humanity-lives.js
+++ src/contents/42-where-75-of-humanity-lives.js
@@ -4,32 +4,25 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      The numbers are rounded to billion people to make it easier to remember.
-      Incomes are in price adjusted PPP 2011 dollars by ICP.
-      <H.A href="http://gapm.io/incm">Gapminder[8]</H.A> estimates that the
-      number of people on each income level in 2017 are as follows:
+      それぞれの数字は覚えやすいように10億人単位で四捨五入している。所得は国の購買力をもとに調整されており、2011年国際ドルを基にしている。それぞれの所得レベルごとの人数の推定は以下の通り
+      (<H.A href="http://gapm.io/incm">Gapminder[8]</H.A>):
     </p>
     <ul>
-      <li>Level 1 has 0.75 billion people living on less than $2 per day</li>
-      <li>
-        Level 2 has 3.3 billion people living on incomes between $2 to $8 per
-        day
-      </li>
-      <li>Level 3 has 2.5 billion people living on $8 to $32 per day</li>
-      <li>Level 4 has 0.9 billion people living on more than $32 per day</li>
+      <li>レベル1: 7億5000万人。1日の所得は2ドル以下。</li>
+      <li>レベル2: 33億人。1日の所得は2ドルから8ドル。</li>
+      <li>レベル3: 25億人。1日の所得は8ドルから32ドル。</li>
+      <li>レベル4: 9億人。1日の所得は32ドル以上。</li>
     </ul>
     <p>
-      The detailed estimates are based on the World Bank's PovcalNet for 2013
-      and forecasts from <H.A href="http://gapm.io/ximfw">IMF[1]</H.A>.
-      <H.A href="http://iresearch.worldbank.org/PovcalNet">Povcal[1]</H.A> is
-      the dataset that the World Bank uses to estimate the official rate of
-      extreme poverty worldwide. Data was collected through household income
-      surveys from across the world. National currencies are converted to
-      comparable dollars adjusted for differences between countries in cost of
-      living. The threshold of $2 per day is almost identical to the World
-      Bank's $1.9 per day. Gapminder rounded that up to $2 per day to make it
-      easier to remember, and to avoid the false precision in poverty estimates
-      that are very rough.
+      世界銀行の
+      <H.A href="http://iresearch.worldbank.org/PovcalNet">
+        PovcalNetの2013年のデータ
+      </H.A>
+      と、<H.A href="http://gapm.io/ximfw">IMF[1]</H.A>
+      の予測を基にしている。世界銀行は、極度の貧困率を算出するのにPovcalNetを使っており、世界中の家庭で所得調査を行った結果が掲載されている。
+    </p>
+    <p>
+      世界銀行は極度の貧困を「1日1.9ドル以下の所得」と定義しており、わたしたちの定義のレベル1と2の境目と近い。だが、1.9ドルというと実際と反して正確な調査である印象をもたらすため、わたしたちは2ドルを境にしている。
     </p>
   </>
 )
