aeebd1522cf446c03b59dc93b01f74b0ce2eac6b
Shu Uesugi <shu@chibicode.com>
2019-03-02 18:52:10 -0800

    Update 52-graphs-incomes-in-mexico-us.js

--- src/contents/52-graphs-incomes-in-mexico-us.js
+++ src/contents/52-graphs-incomes-in-mexico-us.js
@@ -1,36 +1,35 @@
-import React from 'react'
+/** @jsx jsx */
+import {jsx, css} from '@emotion/core'
 import * as H from '../components/h'
 
 export default () => (
   <>
     <p>
-      The graphs showing people distributed by income, comparing Mexico and the
-      United States in 2016, are based on the same data as the four income
-      levels, slightly adjusted to align with the shape of the distributions
-      from the latest available national income surveys.
+      メキシコとアメリカの所得ごとの人口分布(2016年)は、4つの所得レベルを説明したときと同じデータを基に、各国の最新の所得調査をもとにした調整を加えて算出した。
     </p>
     <p>
-      The number of people on different incomes are based on latest available
-      income distribution data from the World Bank's
-      <H.A href="http://iresearch.worldbank.org/PovcalNet">Povcal[1]</H.A>,
-      which was extended to 2013 by{' '}
-      <H.A href="https://gapm.io/ihhinc">Gapminder[10]</H.A> using the indicator
-      Household per capita income and converted into constant international
-      dollars (PPP 2011), on a logarithmic scale. By adjusting the exchange
-      rates to consider price differences with PPP, the incomes are more
-      comparable across countries. The shapes of the curves were adjusted to
-      better follow the distributions, as reported by the latest available
-      national income survey for each country; see ENIGH for Mexico, and US-CPS
-      for the United States.
+      所得ごとの人口は世界銀行の
+      <H.A href="http://iresearch.worldbank.org/PovcalNet">Povcal[1]</H.A>
+      を基にしている。家庭あたりの所得を、購買力の違い(PPP)を考慮し2011年国際ドルに変換した。こうすることで国ごとの所得を正しく比較できる。横軸は対数目盛を利用している。さらに、各国の所得調査のデータ(
+      <H.A href="http://www.beta.inegi.org.mx/proyectos/enchogares/regulares/enigh/nc/2016">
+        ENIGH
+      </H.A>
+      と<H.A href="http://www.gapm.io/xuscps17">US-CPS</H.A>
+      )に沿うように分布の形に調整を加えている。
     </p>
     <p>
-      The right chart uses a doubling scale for income, just like all other
-      charts displaying income the book. This image shows what the shapes would
-      look like if they were drawn on a linear scale. For more details see:
-      <H.A href="http://gapm.io/fbincmlinlog">gapm.io/fbincmlinlog</H.A>
+      本書ではどの所得のグラフでも対数目盛を利用している。対数目盛を使用しない場合、グラフは以下のような形になる。
+      <H.A href="http://gapm.io/fbincmlinlog">詳しくはこちら</H.A>。
     </p>
     <p>
-      <img src="/static/images/sources/us-mexico-linear-graph.jpg" alt="" />
+      <img
+        src="/static/images/sources/us-mexico-linear-graph.jpg"
+        alt=""
+        css={css`
+          max-width: 400px;
+          margin: 0 auto;
+        `}
+      />
     </p>
 
     <H.InlineCitation>
