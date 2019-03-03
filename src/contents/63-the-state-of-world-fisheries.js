e4215eb68fcd80b1b424b2b269e84b5594ffe22e
Shu Uesugi <shu@chibicode.com>
2019-03-02 19:37:58 -0800

    Update 63-the-state-of-world-fisheries.js

--- src/contents/63-the-state-of-world-fisheries.js
+++ src/contents/63-the-state-of-world-fisheries.js
@@ -4,24 +4,20 @@ import * as H from '../components/h'
 export default () => (
   <>
     <p>
-      Fishing vessels are becoming larger and can go out further into the deep
-      seas to find the remaining stocks of fish. In their report The State of
-      World Fisheries and Aquaculture 2016, FAO[2] conducted an analysis of
-      assessed commercial fish stocks. The study showed that the share of fish
-      stocks within biologically sustainable levels decreased from 90 percent in
-      1974, to 68.6 percent in 2013.
+      漁船は年々大型化し、より遠くに出て魚を獲ることができるようになった。FAO[2]の
+      <H.A href="http://www.gapm.io/xfaofi">
+        “The State of World Fisheries and Aquaculture 2016”
+      </H.A>
+      によると、漁業の対象になる魚のうち、種が持続可能な割合は1974年には90%だったが、2013年には68.6%に減った。
     </p>
     <p>
-      In The Plundered Planet (2010), Collier shows how the real price of a
-      natural resource is calculated---the use by one generation of humans,
-      based on the reproduction rate of the resource. This is a way of
-      determining how much fish each generation can consume.
+      ポール・コリアー著『収奪の星─天然資源と貧困削減の経済学』によると、人類の一世代が消費した魚の数と、その魚の増殖のペースを比べることで、本来どのくらいの魚を食べても良いかを知ることができる。
     </p>
     <p>
-      UNEP[1] reports more than 500 recorded dead zones in polluted coastal
-      areas around the world. According to
-      <H.A href="http://gapm.io/xiucnr4">IUCN Red List[4]</H.A>, the number of
-      threatened species in 2015 was 23,250 and increased to 24,307 in 2016.
+      <H.A href="http://www.gapm.io/xpolfr17">UNEP[1]</H.A>
+      は、世界中で500以上の魚が生息できない地帯のリストを公開している。 また
+      <H.A href="http://gapm.io/xiucnr4">IUCN Red List[4]</H.A>
+      によると、絶滅が脅かされている種の数は2015年に2万3250種だったが、2016年には2万4307種になった。
     </p>
 
     <H.InlineCitation>
