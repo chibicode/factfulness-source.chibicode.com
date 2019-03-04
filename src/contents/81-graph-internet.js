17a6e65b4a15dd00ded4887d1e7969c26f3d3a3f
Shu Uesugi <shu@chibicode.com>
2019-03-04 06:57:50 -0800

    Update 81-graph-internet.js

--- src/contents/81-graph-internet.js
+++ src/contents/81-graph-internet.js
@@ -5,15 +5,18 @@ export default () => (
   <>
     <H.GraphThumbnail src="/static/images/32-graphs/g31.png" />
     <p>
-      Data for 2005 to 2017 is taken from{' '}
-      <H.A href="http://gapm.io/xituintern">ITU[2]</H.A> divided by{' '}
-      <H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>. Data before 2005
-      comes from previous editions of the ITU report via World Bank[19]. These
-      are combined in <H.A href="http://gapm.io/dintus">Gapminder[22]</H.A>. The
-      first data point, zero internet users in 1980, is based on the Internet
-      System Consortium (ISC), which count internet hosts historically. The
-      first record is from August 1981 with 213 hosting servers, why we assume
-      that the year before, the number of users was practically zero.
+      2005年から2017年のデータは
+      <H.A href="http://gapm.io/xituintern">ITU[2]</H.A>を世界人口あたり(
+      <H.A href="https://esa.un.org/unpd/wpp">UN-Pop[1]</H.A>
+      )で割ったもの。2005年以前は
+      <H.A href="https://gapm.io/xwb1719">世界銀行のデータ</H.A>
+      をわたしたちがまとめ直した
+      <H.A href="http://gapm.io/dintus">(Gapminder[22])</H.A>。
+    </p>
+    <p>
+      インターネットのホストサーバーの数を記録している
+      <H.A href="https://gapm.io/xitho">ISC</H.A>(Internet Systems
+      Consortium)によると、はじめて記録がつけられたのは1981年8月で、213のホストサーバーがあった。おそらくその1年前の1980年には、人口あたりでみるとネットの利用者はゼロ%に近かっただろう。
     </p>
     <H.GraphExplanation />
   </>
