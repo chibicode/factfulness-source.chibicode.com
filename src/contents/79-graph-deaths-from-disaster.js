d7087bc7dad0c9fd1d8eb8b4ea606dfcd1c2b3c3
Shu Uesugi <shu@chibicode.com>
2019-03-03 20:26:48 -0800

    Update 79-graph-deaths-from-disaster.js

--- src/contents/79-graph-deaths-from-disaster.js
+++ src/contents/79-graph-deaths-from-disaster.js
@@ -2,28 +2,13 @@ import React from 'react'
 import * as H from '../components/h'
 
 export default () => (
-  <p>
-    Annual deaths from natural disasters have decreased by 75 percent over the
-    past 100 years, according to the International Disaster Database; see
-    <H.A href="http://www.emdat.be/">EM-DAT</H.A>. Since disasters vary from
-    year to year, here we compare ten-year averages. In chapter four where we
-    further discuss the decline, we also use 25-year averages. In the last ten
-    years (2007--2016), on average 80,386 people were killed by natural
-    disasters per year. This is 25 percent of the number 100 years earlier
-    (1907--1916), when it was 325,742 deaths per year. The huge decline in
-    disaster deaths would be even more striking if two other major global
-    changes are taken into account. First that the world's population has
-    increased by four, which calls for counting disaster deaths per capita. In
-    1907--1916 there were 181 disaster deaths per million people. In 2007--2016
-    the number was 11. The relative number has dropped to 6 percent of what it
-    was 100 years ago. Second, 100 years ago the communication technologies for
-    reporting disasters and their victims were very primitive, compared to the
-    monitoring of today. Many catastrophes must have gone unrecorded ur been
-    underreported. The EM-DAT data include death toll estimates for 8969
-    disasters recorded worldwide since 1900, including all known emergency
-    events categorized as: Animal accident, Complex Disasters, Drought,
-    Earthquake, Epidemic, Extreme temperature, Flood, Fog, Impact, Insect
-    infestation, Landslide, Mass movement (dry), Storm, Volcanic activity,
-    Wildfire.
-  </p>
+  <>
+    <p>
+      <H.OtherCardLink
+        useTitleAsChildren
+        permalink="11-fact-question-7-natural-disasters"
+      />
+      を参考のこと。
+    </p>
+  </>
 )
