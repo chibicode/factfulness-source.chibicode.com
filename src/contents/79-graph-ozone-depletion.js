83db2f8a9f0ebc5c00ddf74cfbaedc2a58d12551
Shu Uesugi <shu@chibicode.com>
2019-03-03 23:19:00 -0800

    Update 79-graph-ozone-depletion.js

--- src/contents/79-graph-ozone-depletion.js
+++ src/contents/79-graph-ozone-depletion.js
@@ -5,32 +5,23 @@ export default () => (
   <>
     <H.GraphThumbnail src="/static/images/32-graphs/g15.png" />
     <p>
-      The data show the impressive decline in the use of gases that are causing
-      a hole in the ozone layer of the Earth's atmosphere. The ozone layer
-      prevents harmful UVB wavelengths of ultraviolet light from reaching the
-      Earth's surface, where it causes skin cancer, sunburn, and cataracts, all
-      of which were projected to increase dramatically as a result of thinning
-      ozone. The UVB light is also harmful to plants and animals. When
-      scientists presented evidence of the gases causing the ozone hole, the
-      world reacted fast. All countries agreed to stop using these gases, and
-      ratified the Montreal Protocol in 1987. Since then, humanity has almost
-      completely stopped consuming Ozone Depleting Substances (ODS). In 2017 the
-      hole finally showed signs of shrinking as reported by
+      グラフを見ると、オゾン層破壊物質の使用量が急激に減っていることがわかる。オゾン層は日焼け、白内障、皮膚がんの原因になり、植物や動物にも悪影響を与える紫外線を遮断してくれる。
+    </p>
+    <p>
+      科学者たちがオゾンホールの原因となるガスを突き止めたとき、世界は対応を急いだ。1987年にモントリオール議定書が採択され、すべての国がこのガスを使うことをやめた。以来、人類はオゾン層破壊物質(ODS)をほとんど使用しなくなった。2017年になるとオゾンホールが縮小し始めた(ソース:{' '}
       <H.A href="https://news.mit.edu/2016/signs-healing-antarctic-ozone-layer-0630">
         MIT
       </H.A>
-      which may be caused by warmer temperature according to
+      )。縮小の原因は地球温暖化によるものかもしれない(ソース:{' '}
       <H.A href="https://www.nasa.gov/feature/goddard/2017/warm-air-helped-make-2017-ozone-hole-smallest-since-1988">
         NASA
       </H.A>
-      . The trend line shows ODS Consumption in ODP Tonnes. The unit ODP tonnes
-      is not an actual weight measurement, but represent the destructive effects
-      of a substance compared to a reference substance. Data is compiled and
-      available form <H.A href="http://gapm.io/xods17">UNEP[4]</H.A>, and the
-      substances included are Chlorofluorocarbons (CFCs), Halons, Other Fully
-      Halogenated CFCs, Carbon Tetrachloride, Methyl Chloroform,
-      Hydrochlorofluorocarbons (HCFCs), Hydrobromofluorocarbons (HBFCs),
-      Bromochloromethane and Methyl Bromide.
+      )。
+    </p>
+    <p>
+      グラフは使用量を千トンで表しているが、これは実際に重さを測った結果ではなく、他の物質に比べた悪影響を表した数字だ。データは
+      <H.A href="http://gapm.io/xods17">UNEP[4]</H.A>
+      によるもの。オゾン層破壊物質に含まれるのはクロロフルオロカーボン、ハロン、四塩化炭素、メチルクロロホルム、ハイドロクロロフルオロカーボン、ハイドロブロモフルオロカーボン、ブロモクロロメタン、臭化メチルなど。
     </p>
   </>
 )
