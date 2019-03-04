36f95bbc8a8e20cc9c8b16da780af0c76d286f53
Shu Uesugi <shu@chibicode.com>
2019-03-04 04:54:53 -0800

    Update 80-graph-new-music.js

--- src/contents/80-graph-new-music.js
+++ src/contents/80-graph-new-music.js
@@ -5,17 +5,27 @@ export default () => (
   <>
     <H.GraphThumbnail src="/static/images/32-graphs/g20.png" />
     <p>
-      The first time music was "recorded" was for experimental purposes in 1857.
-      Since then, music recording have increased both in terms of quality and
-      quantity. The curve shows the number of songs on Spotify, based on the
-      recording date as stated by the
+      歴史上はじめて音楽が録音されたのは
+      <H.A href="https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A9%E3%83%8E%E3%83%88%E3%82%B0%E3%83%A9%E3%83%95">
+        1857年のこと
+      </H.A>
+      。それ以来、音楽は量と質ともに向上してきた。このグラフはSpotifyに登録されている曲の数を表している。Spotifyの曲データにはレコーディングの日付が入っているが、これは
       <H.A href="http://isrc.ifpi.org/en/faq">ISRC</H.A> (International Standard
-      Recording Code) which is a global identifier for sound recordings that let
-      music artists track and charge based on how often their recordings are
-      played in different channels. Of course, not all music recordings are
-      available through Spotify, but the shape of the curve shows the mind
-      blowing increase in cultural expression and consumption.
+      Recording
+      Code)という、国際的な楽曲データベースによるもの。すべての曲がSpotifyに登録されているわけではないが、それにしてもこのグラフの形はすごい。表現活動と、文化の消費活動が最近になってどれだけ増えたかがわかる。
     </p>
+    <H.BlockAddition>
+      <p>
+        <H.BlockAdditionHeader />
+        Spotifyのデータについての訳者の追記は
+        <H.A href="https://twitter.com/chibicode/status/1086669170597457920">
+          こちら
+        </H.A>
+        。<H.A href="https://barubora3.net/?p=632">こちらの記事</H.A>と
+        <H.A href="https://barubora3.net/?p=600">こちらの記事</H.A>も参考になる
+        。
+      </p>
+    </H.BlockAddition>
     <H.GraphExplanation />
   </>
 )
