ebc7644fdfabae9e5d13e7f1fc966633a97a862a
Shu Uesugi <shu@chibicode.com>
2019-03-05 20:06:45 -0800

    :pencil:

--- src/components/intro-hero.js
+++ src/components/intro-hero.js
@@ -4,6 +4,9 @@ import {useContext} from 'react'
 import {titleArray} from '../lib/meta'
 import Book from './book'
 import {PageContext} from './page'
+import * as H from './h'
+import AmazonLink from './amazon-link'
+import Link from './link'
 
 const IntroHero = () => {
   const {type} = useContext(PageContext)
@@ -38,6 +41,17 @@ const IntroHero = () => {
       >
         <Book />
       </p>
+      <p>
+        これは、2019年1月に日経BPから発売された訳書『
+        <Link LinkComponent={AmazonLink}>ファクトフルネス</Link>
+        』の著者が公開している
+        <H.A href="https://www.gapminder.org/factfulness-book/notes/">
+          ウェブ脚注
+        </H.A>
+        の日本語訳だ。本のp353〜p373にも21ページの脚注があるが、それの拡張版だ。文字数にすると、本の脚注は約2万5000文字なのに対し、このウェブ脚注は7万文字以上ある。
+      </p>
+      <p>本の脚注からの引用</p>
+      <p>GitHub</p>
     </>
   )
 }
