761fb0cd08a4973e8f7224e4060c4bff00e27c6f
Shu Uesugi <shu@chibicode.com>
2019-03-06 16:11:43 -0800

    Update errata

--- src/components/intro-errata.js
+++ src/components/intro-errata.js
@@ -10,22 +10,25 @@ const IntroErrata = () => (
       <Link LinkComponent={AmazonLink}>ファクトフルネス</Link>
       』の正誤表です。
     </p>
-    <p>
-      もし本書に間違いがありましたら、共訳者の上杉 (
-      <Link href="mailto:shu@chibicode.com">shu@chibicode.com</Link>
-      )までご連絡ください。
-    </p>
-    <p>
-      また、
-      <Link LinkComponent={InternalLink} href="/">
-        脚注
-      </Link>
-      や、
-      <Link href="https://jp.chibicode.com/factfulness-notes/">
-        脚注についてのブログ記事
-      </Link>
-      もあわせてご覧ください。
-    </p>
+    <ul>
+      <li>
+        もし本書に間違いがありましたら、共訳者の上杉 (
+        <Link href="mailto:shu@chibicode.com">shu@chibicode.com</Link>
+        )までご連絡ください。
+      </li>
+      <li />
+      <li>
+        また、
+        <Link LinkComponent={InternalLink} href="/">
+          脚注
+        </Link>
+        や、
+        <Link href="https://jp.chibicode.com/factfulness-notes/">
+          脚注についてのブログ記事
+        </Link>
+        もあわせてご覧ください。
+      </li>
+    </ul>
   </>
 )
 
