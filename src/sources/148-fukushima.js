b8d682a33b41aec820d7eed42e42df3f1e3ff7fb
Shu Uesugi <shu@chibicode.com>
2019-03-08 23:16:13 -0800

    Update 148-fukushima.js

--- src/sources/148-fukushima.js
+++ src/sources/148-fukushima.js
@@ -15,9 +15,41 @@ export default () => (
         によると、特に病状が重かった61人の後期高齢者は避難中か避難直後に亡くなった。さらにIchisekiによると、高齢者を多く含む約1600人が避難生活中に亡くなった。
       </H.FB>
     </p>
+    <H.BlockAddition>
+      <p>
+        <H.BlockAdditionHeader />
+        上記のIchisekiの論文はこちらの警視庁の資料を引用している:「
+        <H.A href="http://www.reconstruction.go.jp/topics/4.siryou3.pdf">
+          東日本大震災における震災関連死の死者数
+        </H.A>
+        」(2012年3月31日までに把握できた数)。この資料によると、震災関連死の死者は1632人で、「東日本大震災による負傷の悪化等により亡くなられた方で、災害弔慰金の支給等に関する法律に基づき、当該災害弔慰金の支給対象となった方」と定義されている。
+      </p>
+    </H.BlockAddition>
     <p>
       放射線被ばくで亡くなった人はいない。世界保健機関は「死亡率の微増はあるかもしれないが、それは非常に少数の人々に限られるだろう」と結論づけた。
     </p>
+    <H.BlockAddition>
+      <p>
+        <H.BlockAdditionHeader />
+        原著の執筆時点では死亡者はいなかったが、2018年9月には初めての被ばくによる死亡者が原発作業員から出た。これは厚生省の発表で、
+        <H.A href="https://www.nikkei.com/article/DGXMZO34966870U8A900C1CR8000/">
+          日経新聞
+        </H.A>
+        、
+        <H.A href="https://mainichi.jp/articles/20180905/k00/00m/040/105000c">
+          毎日新聞
+        </H.A>
+        、
+        <H.A href="https://www.asahi.com/articles/ASL9524MNL95UBQU001.html">
+          朝日新聞
+        </H.A>
+        、
+        <H.A href="https://www.yomiuri.co.jp/science/20180904-OYT1T50064/">
+          読売新聞
+        </H.A>
+        が報じた。
+      </p>
+    </H.BlockAddition>
     <p>
       <H.FB pageNumber={364}>
         Pew[1]が2012年に行った調査によると、日本人の76%が福島産の食べ物は危険だと答えた。
@@ -28,26 +60,5 @@ export default () => (
       </H.A>
       には、「福島」という単語自体にまつわる風評被害について書かれている。
     </p>
-    <H.BlockAddition>
-      <p>
-        <H.BlockAdditionHeader />
-        上記のIchisekiの論文はこちらの警視庁の資料を引用している:「
-        <H.A href="http://www.reconstruction.go.jp/topics/4.siryou3.pdf">
-          東日本大震災における震災関連死の死者数
-        </H.A>
-        」(2012年3月31日までに把握できた数)。この資料の中で、「震災関連死の死者」は「東日本大震災による負傷の悪化等により亡くなられた方で、災害弔慰金の支給等に関する法律に基づき、当該災害弔慰金の支給対象となった方」と定義されている。
-      </p>
-      <p>
-        また、本書では引用されていないが、2012年8月に行われた「
-        <H.A href="http://www.reconstruction.go.jp/topics/001188.html">
-          復興庁の災関連死に関する検討会(第３回)
-        </H.A>
-        」によると、
-        <H.A href="http://www.reconstruction.go.jp/topics/3-2_siryou1.pdf">
-          震災関連死で最も多かった原因
-        </H.A>
-        が「避難所等における生活の肉体・精神的疲労」と「避難所等への移動中の肉体・精神的疲労」だった。
-      </p>
-    </H.BlockAddition>
   </>
 )
