768b9ee6988cbf63fb1d1e0d3d0e5475c79de690
Shu Uesugi <shu@chibicode.com>
2019-05-23 10:29:39 -0700

    Create 12-fact-question-10-women-s-education.js

---
+++ pages/embed/12-fact-question-10-women-s-education.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {IndexTheme} from '..'
+import Page from '../../src/components/page'
+
+export default () => (
+  <IndexTheme>
+    <Page type="index" embedSingle="12-fact-question-10-women-s-education" />
+  </IndexTheme>
+)
